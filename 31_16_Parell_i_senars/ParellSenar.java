500849ef222388abcd048e02160595dab2388e79
Angelo Montenegro <angelomontenegro200306@gmail.com>
2022-10-21 00:22:30 +0200

    finalizado 31_16

---
+++ 31_16_Parell_i_senars/ParellSenar.java
@@ -0,0 +1,19 @@
+/*
+ * Programa que indica si el número proporcionat com a primer argument
+ * és parell o senar.
+ * Si no s'especifica cap número, el resultat és indeterminat.
+ *angelo montenegro
+ */
+public class ParellSenar {
+    public static void main(String[] args) {
+        System.out.println("numero?");
+        int numero = Integer.parseInt(Entrada.readLine());
+         if (numero % 2 == 0){
+           System.out.println("el numero  " +  numero  + " es parell");
+         }
+            else {
+              System.out.println("el numero  " +  numero   + " es senar");
+            }
+         
+    }
+}
