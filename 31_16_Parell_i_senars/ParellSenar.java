9f0309ca04be5402becc1559f911cad9d6b6be8d
Angelo Montenegro <angelomontenegro200306@gmail.com>
2022-10-21 23:28:48 +0200

    finalizado 31_17

--- 31_16_Parell_i_senars/ParellSenar.java
+++ 31_16_Parell_i_senars/ParellSenar.java
@@ -6,8 +6,7 @@
  */
 public class ParellSenar {
     public static void main(String[] args) {
-        System.out.println("numero?");
-        int numero = Integer.parseInt(Entrada.readLine());
+       int numero = Integer.parseInt(args[0]);
          if (numero % 2 == 0){
            System.out.println("el numero  " +  numero  + " es parell");
          }
---
+++ 31_17_ternari/TernariParImp.java
@@ -0,0 +1,13 @@
+/*
+ * Programa que indica si el número proporcionat com a primer argument
+ * és parell o senar.
+ * Si no s'especifica cap número, el resultat és indeterminat.
+ */
+public class TernariParImp {
+    public static void main(String[] args) {
+    int numero = Integer.parseInt(args[0]);
+    String resultat = (numero % 2 == 0) ?  "par" : "impar"; 
+       System.out.println("el resultado es :"  +  resultat);
+    }
+           
+}
