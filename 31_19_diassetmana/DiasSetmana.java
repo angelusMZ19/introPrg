cc382f2e65328c918f96473f3812eedc8b3e81c4
Angelo Montenegro <angelomontenegro200306@gmail.com>
2022-10-22 02:25:00 +0200

    finalizado 31_19

---
+++ 31_19_diassetmana/DiasSetmana.java
@@ -0,0 +1,30 @@
+/*angelo
+*tenga fe poco a poc voy avanzando 
+*/
+public class DiasSetmana {
+    public static void main(String[]args){
+    System.out.println("Inserte Nº dia de la setmana:");
+    
+    int diaSemana = Integer.parseInt(Entrada.readLine());
+    
+    if (diaSemana == 1){
+        System.out.println("El numero seleccionado corresponde al dia Lunes");
+        }else if (diaSemana == 2){
+            System.out.println("El numero seleccionado corresponde al dia Martes");
+            }else if (diaSemana == 3){
+                System.out.println("El numero seleccionado corresponde al dia Miercoles");
+                }else if (diaSemana == 4){
+                    System.out.println("El numero seleccionado corresponde al dia Jueves");
+                    }else if (diaSemana == 5){
+                        System.out.println("El numero seleccionado corresponde al dia Viernes");
+                        }else if (diaSemana == 6){
+                            System.out.println("El numero seleccionado corresponde al dia Sabado");
+                            }else if (diaSemana == 7){
+                                System.out.println("El numero seleccionado corresponde al dia Domingo");
+                                } else if (diaSemana > 7){
+                                System.out.println("ERROR");
+                                }
+   }
+}
+        
+          
