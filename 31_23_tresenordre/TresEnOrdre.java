7818c4bf3d15f5dd47de47fa7caf6f860c3e7da1
Angelo Montenegro <angelomontenegro200306@gmail.com>
2022-10-25 01:48:57 +0200

    fianlizado 31_23

--- 31_23_tresenordre/TresEnOrdre.java
+++ 31_23_tresenordre/TresEnOrdre.java
@@ -2,7 +2,7 @@
  * Programa que ordena dos nombres
  *Angelo Montenegro :v
  */
-public class TresEnOrnden {
+public class TresEnOrdre {
     public static void main(String[] args) {
        System.out.println("Primer?");
             int primer = Integer.parseInt(Entrada.readLine());
@@ -16,25 +16,25 @@ public class TresEnOrnden {
                 if (primer < segon){
                     if (primer< tercer){
                         if (segon <tercer){
-                         System.out.println( primer + "  ,  " + segon + "  ,  " + tercer);
+                         System.out.println( primer + ", " + segon + " i " + tercer);
                             }else{  
-                            System.out.println( primer + "  ,  " + tercer + "  ,  " + segon);}
+                            System.out.println( primer + ", " + tercer + " i " + segon);}
                      }
                  }
                  else {
                     if (segon < tercer){
                         if (primer < tercer){
-                         System.out.println( segon + "  ,  " + primer + "  ,  " + tercer);}
+                         System.out.println( segon + ", " + primer + " i " + tercer);}
                         else {
-                         System.out.println( segon + "  ,  " + tercer + "  ,  " + primer);}
+                         System.out.println( segon + ", " + tercer + " i " + primer);}
                       }
                     }
                             
                              if (tercer < primer){
                                  if (segon < primer){
-                                  System.out.println( tercer + "  ,  " + segon + "  ,  " + primer);
+                                  System.out.println( tercer + ", " + segon + " i " + primer);
                                   } else if (primer < segon){
-                                     System.out.println( tercer + "  ,  " + primer + "  ,  " + segon);}
+                                     System.out.println( tercer + ", " + primer + " i " + segon);}
                                  }
                               }
             }
