4221d64f1ee622e82b24ea491ab7a4a5f230cfc5
Angelo Montenegro <angelomontenegro200306@gmail.com>
2022-10-24 12:47:32 +0200

    finalizado 31_24

---
+++ 31_24_calculadora_senzilla/CalculadoraSenzilla.java
@@ -0,0 +1,28 @@
+/*
+ * Aquest programa demana dos operands i un operador binari (+, -, * o /)
+ * i mostra el resultat del càlcul corresponent
+ */
+public class CalculadoraSenzilla {
+    public static void main(String[] args) {
+        System.out.println("Operand1?");
+        double operand1 = Double.parseDouble(Entrada.readLine());
+        System.out.println("Operador?");
+        char operador = Entrada.readLine().charAt(0);
+        System.out.println("Operador2");
+        double operand2 = Double.parseDouble(Entrada.readLine());
+
+        if (operador == '+') {
+            double resultat = operand1 + operand2;
+            System.out.println(operand1 + " + " + operand2 + " = " + resultat);
+        } else if (operador == '-') {
+            double resultat = operand1 - operand2;
+            System.out.println(operand1 + " - " + operand2 + " = " + resultat);
+            } else if (operador == '*') {
+                double resultat = operand1 * operand2;
+                    System.out.println(operand1 + " * " + operand2 + " = " + resultat);
+                } else if (operador == '/') {
+                    double resultat = operand1 / operand2;
+                    System.out.println(operand1 + " / " + operand2 + " = " + resultat);
+                        } 
+           } 
+    } 
