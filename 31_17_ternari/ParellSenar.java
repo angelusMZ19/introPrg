e1edd2b2060a4841f9f03836bfc61e0c4f03e10e
prgtest <angelomontenegro200306@gmail.com>
2022-10-24 23:14:05 +0200

    finalizado 31_17  ∾ prgtest autocommit 31_17 #2

--- 31_17_ternari/ParellSenar.java
+++ 31_17_ternari/ParellSenar.java
@@ -7,7 +7,7 @@ public class ParellSenar {
     public static void main(String[] args) {
     int numero = Integer.parseInt(args[0]);
     String resultat = (numero % 2 == 0) ?  "parell" : "senar"; 
-       System.out.println("el numero" + numero + " és " +  resultat);
+       System.out.println("el numero " + numero + " és " +  resultat);
     }
            
 }
