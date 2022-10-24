b01fc4a0b00d77d8ce40b2baa0451eb22a8c687f
prgtest <angelomontenegro200306@gmail.com>
2022-10-25 01:35:46 +0200

    finalizado 31_22  ∾ prgtest resultat 31_22: FALLA

--- .introprg/tmp/test/MesosAnys.java
+++ .introprg/tmp/test/MesosAnys.java
@@ -15,29 +15,29 @@ public class MesosAnys {
       int anyDes= any + 1;
       
        if (mes == 1){
-       System.out.println("Mes anterior "+ "12" +"/"+ anyAnt + "  i " + "mes següent" + mesDes + "  /  " + any );
+       System.out.println("Mes anterior "+ "12" +"/"+ anyAnt + "  i " + "mes següent" + mesDes + "/" + any );
         }else if (mes == 2){
-            System.out.println("Mes anterior "+ mesAnt +"/"+ any + " i  " + "mes següent " + mesDes + "  /  " + any );}
+            System.out.println("Mes anterior "+ mesAnt +"/"+ any + " i  " + "mes següent " + mesDes + "/" + any );}
             else if (mes == 3){
-                System.out.println("Mes anterior "+ mesAnt +"/"+ any + " i " + "mes següent " + mesDes + "  /  " + any );}
+                System.out.println("Mes anterior "+ mesAnt +"/"+ any + " i " + "mes següent " + mesDes + "/" + any );}
                 else if (mes == 4){
-                    System.out.println("Mes anterior "+ mesAnt +"/"+ any + " i " + "mes següent " + mesDes + "  /  " + any );}
+                    System.out.println("Mes anterior "+ mesAnt +"/"+ any + " i " + "mes següent " + mesDes + "/" + any );}
                     else if (mes == 5){
-                        System.out.println("Mes anterior "+ mesAnt +"/"+ any + " i " + "mes següent " + mesDes + "  /  " + any );}
+                        System.out.println("Mes anterior "+ mesAnt +"/"+ any + " i " + "mes següent " + mesDes + "/" + any );}
                         else if (mes == 6){
-                            System.out.println("Mes anterior "+ mesAnt +"/"+ any + " i " + "mes següent " + mesDes + "  /  " + any );}
+                            System.out.println("Mes anterior "+ mesAnt +"/"+ any + " i " + "mes següent " + mesDes + "/" + any );}
                             else if (mes == 7){
-                                System.out.println("Mes anterior "+ mesAnt +"/"+ any + " i " + "mes següent " + mesDes + "  /  "  + any );}
+                                System.out.println("Mes anterior "+ mesAnt +"/"+ any + " i " + "mes següent " + mesDes + "/"  + any );}
                                 else if (mes == 8){
-                                    System.out.println("Mes anterior "+ mesAnt +"/"+ any + " i " +  "mes següent " + mesDes + "  /  " + any );}
+                                    System.out.println("Mes anterior "+ mesAnt +"/"+ any + " i " +  "mes següent " + mesDes + "/" + any );}
                                     else if (mes == 9){
-                                        System.out.println("Mes anterior "+ mesAnt +"/"+ any +  "   " + "mes següent " + mesDes + "  /  " + any );}
+                                        System.out.println("Mes anterior "+ mesAnt +"/"+ any +  "   " + "mes següent " + mesDes + "/" + any );}
                                             else if (mes == 10){
-                                            System.out.println("Mes anterior "+ mesAnt +"/"+ any + " i " + "mes següent " + mesDes + "  /  " + any );}
+                                            System.out.println("Mes anterior "+ mesAnt +"/"+ any + " i " + "mes següent " + mesDes + "/" + any );}
                                                 else if (mes == 11){
-                                                    System.out.println("Mes anterior "+ mesAnt +"/"+ any + " i " + "mes següent " + mesDes + "  /  " + any );}
+                                                    System.out.println("Mes anterior "+ mesAnt +"/"+ any + " i " + "mes següent " + mesDes + "/" + any );}
                                                     else if (mes == 12){
-                                                        System.out.println("Mes anterior "+ mesAnt + "/"+ any + " i " + "mes següent " + " 1  "+ "  /  "  + anyDes );}
+                                                        System.out.println("Mes anterior "+ mesAnt + "/"+ any + " i " + "mes següent " + " 1  "+ "/"  + anyDes );}
                          
             
             
