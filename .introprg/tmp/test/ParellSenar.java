ec1fdf9a7f6d7c493b3fc43d559d441d6d883832
prgtest <angelomontenegro200306@gmail.com>
2022-10-24 23:11:42 +0200

    finalizado 31_17  ∾ prgtest resultat 31_17: FALLA

--- .introprg/tmp/test/ParellSenar.java
+++ .introprg/tmp/test/ParellSenar.java
@@ -2,17 +2,12 @@
  * Programa que indica si el número proporcionat com a primer argument
  * és parell o senar.
  * Si no s'especifica cap número, el resultat és indeterminat.
- *angelo montenegro
  */
 public class ParellSenar {
     public static void main(String[] args) {
-       int numero = Integer.parseInt(args[0]);
-         if (numero % 2 == 0){
-           System.out.println("El número " +  numero  + " és parell");
-         }
-            else {
-              System.out.println("El número " +  numero   + " és senar");
-            }
-         
+    int numero = Integer.parseInt(args[0]);
+    String resultat = (numero % 2 == 0) ?  "par" : "impar"; 
+       System.out.println("el resultado es :"  +  resultat);
     }
+           
 }
