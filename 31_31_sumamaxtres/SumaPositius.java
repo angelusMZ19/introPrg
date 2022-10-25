50d0469e998d6b446577cbe030525b97e01b4a0f
Angelo Montenegro <angelomontenegro200306@gmail.com>
2022-10-26 01:50:07 +0200

    finalizado 31_31

---
+++ 31_31_sumamaxtres/SumaPositius.java
@@ -0,0 +1,31 @@
+public class SumaPositius {
+    public static void main(String[] args) {
+
+        // creem la variable on guardarem el resultat de sumar
+        int suma = 0;   // inicialment no hem sumat res i per tant és 0
+
+        // declarem la variable que contindrà els valors llegits
+        int valor;
+
+        // processem el primer valor
+        System.out.println("Introdueix primer valor");
+        valor = Integer.parseInt(Entrada.readLine());
+        if (valor >= 0) {
+            suma = suma + valor;
+
+            // processem el segon valor
+            System.out.println("Introdueix segon valor");
+            valor = Integer.parseInt(Entrada.readLine());
+            if (valor >= 0) {
+                suma = suma + valor;
+                
+                System.out.println("Introdueix tercer valor");
+                valor = Integer.parseInt(Entrada.readLine());
+                if (valor >= 0) {
+                suma = suma + valor;
+            }
+        }
+        // mostrem el resultat
+        System.out.println("La suma és " + suma);
+    }
+}
