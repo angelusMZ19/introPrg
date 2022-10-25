b234249d0aad179d435c92719175ace19e47841b
prgtest <angelomontenegro200306@gmail.com>
2022-10-25 23:30:01 +0200

    finalizado 31_28  ∾ prgtest resultat 31_28: FALLA

--- .introprg/tmp/test/Ascensor.java
+++ .introprg/tmp/test/Ascensor.java
@@ -12,24 +12,27 @@ public class Ascensor{
        
         
         
-                    
+        if(pis.equals("planta baixa") && !(botó.equals("pujar un"))|| !(botó.equals("pujar dos"))) {
+            System.out.println("error");}            
         if(pis.equals("planta baixa") && botó.equals("pujar un")) {
             System.out.println("primer pis");
         }else if(pis.equals("planta baixa") && botó.equals("pujar dos")) {
                 System.out.println("segon pis");}
-                    else if(pis.equals("planta baixa") && botó.equals("baixar un")) {
+                    /*else if(pis.equals("planta baixa") && botó.equals("baixar un")) {
                     System.out.println("error");}
                         else if(pis.equals("planta baixa") && botó.equals("baixar dos")) {
-                        System.out.println("error");}
+                        System.out.println("error");}*/
                 
+        if(pis.equals("primer pis") && !(botó.equals("baixar un"))|| !(botó.equals("pujar dos"))) {
+            System.out.println("error");}
         if(pis.equals("primer pis") && botó.equals("pujar un")) {
             System.out.println("segon pis");}
             else if(pis.equals("primer pis") && botó.equals("baixar un")) {
                 System.out.println("planta baixa");}
-                    else if(pis.equals("primer pis") && botó.equals("baixar dos")) {
+                    /*else if(pis.equals("primer pis") && botó.equals("baixar dos")) {
                     System.out.println("error");}
                         else if(pis.equals("primer pis") && botó.equals("pujar dos")) {
-                        System.out.println("error");}
+                        System.out.println("error");}*/
                 
         if(pis.equals("segon pis") && !(botó.equals("baixar un"))|| !(botó.equals("Baixar dos"))) {
             System.out.println("error");}
