4a43d68da8e537c5f5200d1aa0d36cc7d2763d03
prgtest <angelomontenegro200306@gmail.com>
2022-10-26 00:17:07 +0200

    finalizado 31_28  ∾ prgtest resultat 31_28: FALLA

--- .introprg/tmp/test/Ascensor.java
+++ .introprg/tmp/test/Ascensor.java
@@ -38,7 +38,7 @@ public class Ascensor{
                     else if(pis.equals("segon pis") && botó.equals("baixar dos")) {
                     System.out.println("planta baixa");}
                
-        if (!(pis.equals ("planta baixa")) || !(pis.equals  ("primer pis"))||!(pis.equals  ("segon pis"))) {
+      if (pis.equals("sotan")){
                     System.out.println("error");}
                     
        
