f340762277ed7e126542721854e5a075bf92edef
prgtest <angelomontenegro200306@gmail.com>
2022-10-25 23:46:56 +0200

    finalizado 31_28  ∾ prgtest autocommit 31_28 #22

--- 31_28_ascensor/Ascensor.java
+++ 31_28_ascensor/Ascensor.java
@@ -23,15 +23,15 @@ public class Ascensor{
                         /*else if(pis.equals("planta baixa") && botó.equals("baixar dos")) {
                         System.out.println("error");}*/
                 
-        if(pis.equals("primer pis") && !(botó.equals("baixar un"))|| !(botó.equals("pujar dos"))) {
-            System.out.println("error");}
+        /*if(pis.equals("primer pis") && !(botó.equals("baixar un"))|| !(botó.equals("pujar dos"))) {
+            System.out.println("error");}*/
         if(pis.equals("primer pis") && botó.equals("pujar un")) {
             System.out.println("segon pis");}
             else if(pis.equals("primer pis") && botó.equals("baixar un")) {
                 System.out.println("planta baixa");}
-                    /*else if(pis.equals("primer pis") && botó.equals("baixar dos")) {
+                    else if(pis.equals("primer pis") && botó.equals("pujar dos")||botó.equals("baixar dos") || botó.equals("baixar tres")||botó.equals("pujar tres")) {
                     System.out.println("error");}
-                        else if(pis.equals("primer pis") && botó.equals("pujar dos")) {
+                        /*else if(pis.equals("primer pis") && botó.equals("pujar dos")) {
                         System.out.println("error");}*/
                 
         if(pis.equals("segon pis") && !(botó.equals("baixar un"))|| !(botó.equals("baixar dos"))) {
