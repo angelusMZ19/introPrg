2d5af731ecd81fde7930d842139e22d56dbf2911
prgtest <angelomontenegro200306@gmail.com>
2022-10-25 19:38:04 +0200

    finalizado 31_24  ∾ prgtest resultat 31_26: FALLA

--- .introprg/tmp/test/Semafor.java
+++ .introprg/tmp/test/Semafor.java
@@ -7,8 +7,8 @@ public class Semafor {
        
         String color = Entrada.readLine();
         if (color.equals("vermell")) {
-            System.out.println("Detente");
-                } else if (color.equals("amarillo")) {
+            System.out.println("espera");
+                } else if (color.equals("groc")) {
                     System.out.println("Corre!");
                         } else if (color.equals("verde")) {
                             System.out.println("Cruza");}
