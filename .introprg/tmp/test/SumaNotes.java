fd1b951e01f387ab91db285d532ee364b91bb053
prgtest <angelomontenegro200306@gmail.com>
2022-10-26 20:21:33 +0200

    finalizado 31_32  ∾ prgtest resultat 31_32: FALLA

---
+++ .introprg/tmp/test/SumaNotes.java
@@ -0,0 +1,24 @@
+
+
+public class SumaNotes {
+    public static void main(String[] args) {
+
+        // creem la variable on guardarem el resultat de sumar
+        int suma = 0;   // inicialment no hem sumat res i per tant és 0
+
+        // demana el primer valor
+        System.out.println("Introdueix valor");
+        int valor = Integer.parseInt(Entrada.readLine());
+
+        // ves sumant mentre el valor sigui positiu
+        while (valor >= 0 && valor <= 100) {
+            suma = suma + valor;
+            // demana un nou valor
+            System.out.println("Introdueix valor");
+            valor = Integer.parseInt(Entrada.readLine());
+        }
+
+        // mostrem el resultat
+        System.out.println("La suma és " + suma);
+    }
+}
