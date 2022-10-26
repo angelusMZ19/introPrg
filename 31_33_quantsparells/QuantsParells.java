362a38e982d5fb9f3b1f7ed0ff15b83dbe822c90
Angelo Montenegro <angelomontenegro200306@gmail.com>
2022-10-26 20:44:04 +0200

    finalizado 31_33

---
+++ 31_33_quantsparells/QuantsParells.java
@@ -0,0 +1,14 @@
+public class QuantsParells {
+    public static void main(String[] args) {
+        int parell = 0;
+        int valor = 0;
+        while (valor >= 0) {
+            System.out.println("Introdueix valor");
+            valor = Integer.parseInt(Entrada.readLine());
+            if (valor % 2 == 0) {
+                parell++ ;
+            }
+        }
+        System.out.println("Nombre de parells introduïts: " + parell);
+    }
+}
