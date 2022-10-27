f090572c1e46b73270d170ec5fb2063a16116086
prgtest <angelomontenegro200306@gmail.com>
2022-10-27 13:21:45 +0200

    finalizado 31_20  ∾ prgtest autocommit 31_20 #2

--- 31_20_diessetmana_switch/DiaSetmanaSwitch.java
+++ 31_20_diessetmana_switch/DiaSetmanaSwitch.java
@@ -9,34 +9,34 @@ public class DiaSetmanaSwitch {
     
     int diaSemana = Integer.parseInt(Entrada.readLine());
      if ( diaSemana < 1 || diaSemana> 7){ 
-            System.out.println("Error");}
+            System.out.println("Error");
    
-    switch (diaSemana){
-   
-        case 1 : 
-            System.out.println("Dilluns");
-            break;
-        case 2 : 
-            System.out.println("Dimarts");
-            break;
-        case 3 : 
-            System.out.println("Dimecres");
-            break;
-        case 4 : 
-            System.out.println("Dijous");
-            break;
-        case 5 : 
-            System.out.println("Divendres");
-            break;
-        case 6 : 
-            System.out.println("Dissabte");
-            break;
-        case 7 : 
-            System.out.println("Diumenge");
-            break;
-            
+        switch (diaSemana){
+       
+            case 1 : 
+                System.out.println("Dilluns");
+                break;
+            case 2 : 
+                System.out.println("Dimarts");
+                break;
+            case 3 : 
+                System.out.println("Dimecres");
+                break;
+            case 4 : 
+                System.out.println("Dijous");
+                break;
+            case 5 : 
+                System.out.println("Divendres");
+                break;
+            case 6 : 
+                System.out.println("Dissabte");
+                break;
+            case 7 : 
+                System.out.println("Diumenge");
+                break;
+                
+            }
         }
-        
    }
 }
         
