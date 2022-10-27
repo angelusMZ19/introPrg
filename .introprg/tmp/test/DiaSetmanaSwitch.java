6187556edef89a84164d878222e0f6a9c3d4df02
prgtest <angelomontenegro200306@gmail.com>
2022-10-27 13:19:31 +0200

    finalizado 31_20  ∾ prgtest resultat 31_20: FALLA

---
+++ .introprg/tmp/test/DiaSetmanaSwitch.java
@@ -0,0 +1,41 @@
+/*
+*angelo montenegro
+*voy avanzando
+*/
+
+public class DiaSetmanaSwitch {
+    public static void main(String[]args){
+    System.out.println("Inserte Nº dia de la setmana:");
+    
+    int diaSemana = Integer.parseInt(Entrada.readLine());
+    
+    switch (diaSemana){
+        case 1 : 
+            System.out.println("Dilluns");
+            break;
+        case 2 : 
+            System.out.println("Dimarts");
+            break;
+        case 3 : 
+            System.out.println("Dimecres");
+            break;
+        case 4 : 
+            System.out.println("Dijous");
+            break;
+        case 5 : 
+            System.out.println("Divendres");
+            break;
+        case 6 : 
+            System.out.println("Dissabte");
+            break;
+        case 7 : 
+            System.out.println("Diumenge");
+            break;
+            
+        }
+        if ( diaSemana < 1 || diaSemana> 7){ 
+            System.out.println("Error");}
+   }
+}
+        
+          
