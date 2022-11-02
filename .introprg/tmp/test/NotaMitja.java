bdf200cc8490041bfa398939bf013571796a77b2
prgtest <angelomontenegro200306@gmail.com>
2022-11-02 19:48:08 +0100

    finalizado 31_39  ∾ prgtest resultat 31_37: FALLA

---
+++ .introprg/tmp/test/NotaMitja.java
@@ -0,0 +1,32 @@
+public class NotaMitja {
+    public static void main(String[]args){
+     // creem la variable on guardarem el resultat de sumar
+        double suma = 0;   // inicialment no hem sumat res i per tant és 0
+        double  promedio =  0.0  ;
+        double n = 0;
+        double secuencia= 1;
+        
+        // demana el primer valor
+        System.out.println("Introdueix un valor");       
+        double valor = Double.parseDouble(Entrada.readLine());             
+                       
+        // ves sumant mentre el valor sigui positiu
+        while (valor >= 0 && valor <= 100) {
+        
+            suma = suma + valor;
+            n= Math.abs (secuencia);
+            promedio= suma / n;
+            secuencia += 1;
+            // demana un nou valor
+             System.out.println("Introdueix un valor");
+             valor = Double.parseDouble(Entrada.readLine());           
+            }
+            System.out.println(" La mitja de les notes vàlides és " + promedio);
+            }
+           // mostrem el resultat
+           
+    }
+     
+         
+
+
--- 31_40_endevina_cancel/EndevinaNombre.java
+++ 31_40_endevina_cancel/EndevinaNombre.java
@@ -1,3 +1,8 @@
+/* olvemos al ejercicio que implementaba el juego de adivinar
+En esta ocasión vamos a desarrollar una nueva versión que mejorará el ejercicio.
+La versión anterior obligaba a los usuarios a acertar para poder finalizar.
+Esta nueva versión ofrece la posibilidad de salir del juego al introducir una cadena vacía (recuerda isEmpty()). Cuando los usuarios entren la cadena vacía, el programa responderá con el mensaje "Cancelado!" y finalizará.*/
+
 public class EndevinaNombre{
     public static void main (String[]args){
     
