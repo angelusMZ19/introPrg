e4c473a178d275d562fee1431991ed07798a0433
prgtest <angelomontenegro200306@gmail.com>
2022-11-02 19:57:40 +0100

    finalizado 31_39  ∾ prgtest resultat 31_37: FALLA

--- .introprg/tmp/test/NotaMitja.java
+++ .introprg/tmp/test/NotaMitja.java
@@ -2,7 +2,7 @@ public class NotaMitja {
     public static void main(String[]args){
      // creem la variable on guardarem el resultat de sumar
         double suma = 0;   // inicialment no hem sumat res i per tant és 0
-        double  promedio =  0.0  ;
+        double promedio =  0.0  ;
         double n = 0;
         double secuencia= 1;
         
@@ -21,11 +21,16 @@ public class NotaMitja {
              System.out.println("Introdueix un valor");
              valor = Double.parseDouble(Entrada.readLine());           
             }
+            if (suma > 0){
             System.out.println(" La mitja de les notes vàlides és " + promedio);
-            }
+                }else if(valor < 0 || valor > 100){
+                System.out.println("Cap nota vàlida introduïda");
+                }
+    }
+}
            // mostrem el resultat
            
-    }
+
      
          
 
