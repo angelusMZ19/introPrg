38284e48197c1cbc17f53f8994726f39e0cc7d10
prgtest <angelomontenegro200306@gmail.com>
2022-11-02 11:42:21 +0100

    Finalizado 31_39  ∾ prgtest resultat 31_39: FALLA

--- .introprg/tmp/test/EndevinaNombre.java
+++ .introprg/tmp/test/EndevinaNombre.java
@@ -8,12 +8,8 @@ public class EndevinaNombre{
     valor = Integer.parseInt(Entrada.readLine());
     
     while(valor >= 0 && valor <= 100){
-        if (valor == 42){ 
-        System.out.println("Has encertat");
-        
-         System.out.println("Introdueix un valor");
-         valor = Integer.parseInt(Entrada.readLine());        
-         }else if(valor <= 0){
+        while(valor != 42){ 
+         if(valor <= 0){
          System.out.println("Com minim 1");
             
             System.out.println("Introdueix un valor");
@@ -30,11 +26,17 @@ public class EndevinaNombre{
                         valor = Integer.parseInt(Entrada.readLine());
                         }else if(valor > numPensado){
                          System.out.println("Es mes petite que " + valor);
-                           }
-                         }                          
+                            
+                            System.out.println("Introdueix un valor");
+                            valor = Integer.parseInt(Entrada.readLine());
+                            }
+                          }
+                         }
+                 if(valor == 42){
+                 System.out.println("Has encertat");                          
                      }
-                 
+                     
              }
              
-
+}
             
