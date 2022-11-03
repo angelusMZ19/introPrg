25565bc16b96dac1161255b5a7325cf5834174dd
prgtest <angelomontenegro200306@gmail.com>
2022-11-03 17:23:06 +0100

    finalizado 31_44  ∾ prgtest resultat 31_44: PASSA

---
+++ .introprg/tmp/test/EntersIniciFi.java
@@ -0,0 +1,33 @@
+/*Desarrolla un programa que solicite tres números enteros, y escriba los valores entre el primero y el segundo saltando de uno al siguiente con el tercero.
+*/
+public class EntersIniciFi{
+    public static void main (String[]args){
+    
+    
+ 
+    
+    System.out.println("Valor inicial?");
+    int v1 = Integer.parseInt(Entrada.readLine());
+    
+    System.out.println("Valor final?");
+    int v2 = Integer.parseInt(Entrada.readLine());
+    
+    System.out.println("Salt?");
+    int v3 = Integer.parseInt(Entrada.readLine());
+    
+  
+    while(v1 <= v2 && v3 > 0){
+    System.out.println(v1);
+            v1= v1 + v3 ;
+            
+     
+            }
+           
+        }
+        
+ }
+
+
+     
+     
+     
---
+++ 31_48_de_principi_a_fi_for/EntersIniciFi.java
@@ -0,0 +1,22 @@
+/*
+*/
+/*Desarrolla un programa que solicite tres números enteros, y escriba los valores entre el primero y el segundo saltando de uno al siguiente con el tercero.
+*/
+public class EntersIniciFi{
+    public static void main (String[]args){    
+  
+    for ( System.out.println("Valor inicial?");
+          int v1 = Integer.parseInt(Entrada.readLine());
+          System.out.println("Valor final?");
+          int v2 = Integer.parseInt(Entrada.readLine());
+          System.out.println("Salt?");
+          int v3 = Integer.parseInt(Entrada.readLine());
+          v1 <= v2 && v3 > 0; 
+          v1= v1 + v3 ){
+          System.out.println(v1);     
+            }
+           
+        }
+        
+ }
+
