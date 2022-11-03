31f77e3ff8033ee32b54b425718791caf154ed5f
prgtest <angelomontenegro200306@gmail.com>
2022-11-03 16:37:23 +0100

    Finalizado 31_45  ∾ prgtest resultat 31_45: PASSA

---
+++ .introprg/tmp/test/DeuPrimers.java
@@ -0,0 +1,16 @@
+/*
+Hagamos una nueva versión del ejercicio de los diez primeros naturales, 
+esta vez con el bucle for.
+*/
+
+public class DeuPrimers{
+    public static void main (String[]args){
+                 
+        for (int numero = 1;
+             numero <= 10;
+             numero = numero + 1) {  
+             System.out.println(numero);  
+            
+        }
+    }
+}
