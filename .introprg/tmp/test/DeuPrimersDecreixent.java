318966d95b12ad223dbe1db4fd6376ef6866e6f2
prgtest <angelomontenegro200306@gmail.com>
2022-11-03 01:21:16 +0100

    finalizado 31_42  ∾ prgtest resultat 31_42: PASSA

---
+++ .introprg/tmp/test/DeuPrimersDecreixent.java
@@ -0,0 +1,10 @@
+public class DeuPrimersDecreixent{
+    public static void main (String[]args){
+    
+        int numero = 10;         // esta es la variable de recorrido asignandole un valor 
+        while (numero > 0) {  // la condicion que al cumplorse eejcutara los valores hasta llegar al limite que es el 10 
+            System.out.println(numero); //impresion de los numeros 
+            numero = numero - 1;    // imprimira los numeros de un en uno debido a que se le suma uno 
+        }
+    }
+}
