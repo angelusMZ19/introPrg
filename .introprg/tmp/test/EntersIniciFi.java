03759358fc8b684f6efbabfee6845e4d25cde169
prgtest <angelomontenegro200306@gmail.com>
2022-11-04 12:13:21 +0100

    finalizado 31_48  ∾ prgtest resultat 31_48: PASSA

--- .introprg/tmp/test/EntersIniciFi.java
+++ .introprg/tmp/test/EntersIniciFi.java
@@ -1,11 +1,11 @@
+/*
+*/
 /*Desarrolla un programa que solicite tres números enteros, y escriba los valores entre el primero y el segundo saltando de uno al siguiente con el tercero.
 */
 public class EntersIniciFi{
-    public static void main (String[]args){
-    
-    
- 
-    
+    public static void main (String[]args){    
+     
+      
     System.out.println("Valor inicial?");
     int v1 = Integer.parseInt(Entrada.readLine());
     
@@ -15,19 +15,9 @@ public class EntersIniciFi{
     System.out.println("Salt?");
     int v3 = Integer.parseInt(Entrada.readLine());
     
-  
-    while(v1 <= v2 && v3 > 0){
-    System.out.println(v1);
-            v1= v1 + v3 ;
-            
-     
-            }
-           
-        }
-        
- }
-
-
-     
-     
-     
+          
+        for ( v1 = v1; v1 <= v2; v1= v1 + v3){
+         System.out.println(v1);     
+         }
+    }  
+}
