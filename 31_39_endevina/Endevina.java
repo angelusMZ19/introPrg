abdd3d6f96a5b7799c029b2e63b6ebf3fa31d39f
prgtest <angelomontenegro200306@gmail.com>
2022-11-04 23:16:22 +0100

    Finalizado 31_49  ∾ prgtest resultat 31_49: FALLA

---
+++ 31_39_endevina/Endevina.java
@@ -0,0 +1,36 @@
+public class Endevina {
+     public static void main(String[]args){
+     
+     int numPensado = 42;
+     int valor = 0;
+     System.out.println("Introdueix un valor");
+        valor = Integer.parseInt(Entrada.readLine());
+        
+     while (valor != 42) {
+          
+            if (valor < numPensado) {
+              System.out.println("És més gran que " + valor) ;
+               
+                System.out.println("Introdueix un valor");
+                valor = Integer.parseInt(Entrada.readLine());
+           
+                    if (valor > numPensado) {
+                    System.out.println("És més petite que " + valor) ;
+              
+                        System.out.println("Introdueix un valor");
+                        valor = Integer.parseInt(Entrada.readLine());   
+           
+                         
+                        
+                      }   
+                    }
+                    
+                 }
+                 if (valor == numPensado) 
+                         System.out.println("Has encertar!") ;        
+                 
+       }
+}
+ 
+   
+   
