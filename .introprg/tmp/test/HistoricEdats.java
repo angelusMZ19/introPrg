6c38c0e60f41b7f79a4654a64ff6a1640cf0aeb7
prgtest <angelomontenegro200306@gmail.com>
2022-11-08 20:36:58 +0100

    finalizado 31_51  ∾ prgtest resultat 31_51: FALLA

--- .introprg/tmp/test/HistoricEdats.java
+++ .introprg/tmp/test/HistoricEdats.java
@@ -12,9 +12,10 @@ public class HistoricEdats{
    
     int anyNacer= any - edat;
     int edad= 1;
-    if (edat > 0){
+    if (edat <= 1){
         System.out.println("El " + anyNacer + " vau néixer");
-        System.out.println("El " + (anyNacer + 1) + " teníeu 1 any");
+        } else {
+            System.out.println("El " + (anyNacer + 1) + " teníeu 1 any");}
     
        for (int anySecuencia = anyNacer +2 ;
             anySecuencia < any;
@@ -24,7 +25,8 @@ public class HistoricEdats{
                 System.out.println("El " + anySecuencia + " teníeu " + (edad) + " anys");
                 }
                 System.out.println("Adéu " + nombre);
-        }else{
-        System.out.println("Adéu " + nombre);}
+       // } //else {
+       // System.out.println("Adéu " + nombre);
+        }
     }
- }
+ 
