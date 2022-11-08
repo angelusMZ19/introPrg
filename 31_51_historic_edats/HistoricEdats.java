9e0755c9bd473525bae2a61cf3eeb18da634ccd3
Angelo Montenegro <angelomontenegro200306@gmail.com>
2022-11-08 20:23:19 +0100

    finalizado 31_51

---
+++ 31_51_historic_edats/HistoricEdats.java
@@ -0,0 +1,28 @@
+public class HistoricEdats{
+    public static void main (String[]args){
+    
+   System.out.println("El vostre nom?");
+    String nombre = Entrada.readLine();
+            
+   System.out.println("La vostra edat?");
+    int edat = Integer.parseInt(Entrada.readLine());
+   
+   System.out.println("L'any actual?");
+    int any = Integer.parseInt(Entrada.readLine());
+   
+    int anyNacer= any - edat;
+    int edad= 1;
+    if (edat > 0){
+        System.out.println("El " + anyNacer + " vay néixer");
+        System.out.println("El " + (anyNacer + 1) + " tenieu 1 any");
+    
+       for (int anySecuencia = anyNacer +2 ;
+            anySecuencia < any;
+            anySecuencia = anySecuencia + 1){
+            edad= edad +1; 
+      
+                System.out.println("El " + anySecuencia + " tenieu " + (edad) + " anys ");}
+        }else{
+        System.out.println("Adeu " + nombre);}
+    }
+ }
