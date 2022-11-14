67f0b849c6c8630a0d562ee9ee642cfefee4afc6
prgtest <angelomontenegro200306@gmail.com>
2022-11-14 15:50:47 +0100

    finalizado 31_52  ∾ prgtest autocommit 31_52 #1

--- 31_52_quadrat_nombres/QuadratNombres.java
+++ 31_52_quadrat_nombres/QuadratNombres.java
@@ -8,14 +8,18 @@ public class QuadratNombres{
     
     System.out.println("Valor final?");
     int valor = Integer.parseInt(Entrada.readLine());
+    
+    if (valor <= 0){
+        System.out.println("Valor inadequat");
+        }else if (valor >= 1){
 
-    for (int fila = 1; fila <= valor; fila = fila + 1) {
-        for (int columna = 1; columna <= valor; columna= columna + 1)
-            System.out.print(columna);
-            
-            System.out.println( );}
+            for (int fila = 1; fila <= valor; fila = fila + 1) {
+                for (int columna = 1; columna <= valor; columna= columna + 1)
+                    System.out.print(columna);
+                    
+                    System.out.println( );}
      }
   }
-
+}
 
 
