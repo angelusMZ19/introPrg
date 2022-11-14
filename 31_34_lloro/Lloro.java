f77b11bc79084628a10076ab332b2e5e001bd5ad
Angelo Montenegro <angelomontenegro200306@gmail.com>
2022-11-14 16:45:15 +0100

    agregè una capçalera

--- 31_34_lloro/Lloro.java
+++ 31_34_lloro/Lloro.java
@@ -1,3 +1,5 @@
+/*Desenvolupa un programa que vagi demanant paraules i les repeteixi fins que rebi una resposta en blanc o bé buida. És a dir, la resposta sigui un return directament o bé només espais.
+*/
 public class Lloro {
     public static void main(String[] args) {
        
