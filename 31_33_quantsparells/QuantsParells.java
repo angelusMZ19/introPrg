5d93685a25d1fce83e51bef17626ee1af9e47346
Angelo Montenegro <angelomontenegro200306@gmail.com>
2022-11-14 16:42:33 +0100

    agrege capçalera

--- 31_33_quantsparells/QuantsParells.java
+++ 31_33_quantsparells/QuantsParells.java
@@ -1,3 +1,5 @@
+/*Desenvolupa un programa que vagi demanant enters positius i que, en el moment que rebi un de negatiu, mostri el nombre de valors parells introduïts i finalitzi l'execució.
+*/
 public class QuantsParells {
     public static void main(String[] args) {
         int parell = 0;
