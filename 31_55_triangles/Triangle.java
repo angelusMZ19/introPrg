5527696ff3002178a5675a2bb85830397c09cc8b
Angelo Enrique Montenegro Zavala <cf22angelo.montenegro@iesjoandaustria.org>
2022-11-21 18:08:09 +0100

    finalizado 31_55

--- 31_55_triangles/Triangle.java
+++ 31_55_triangles/Triangle.java
@@ -2,27 +2,28 @@
 */
 public class Triangle {
     public static void main (String[]args){
-    System.out.println("quants?");
-    int quant= Integer.parseInt(Entrada.readLine());
+        System.out.println("quants?");
+        int quant= Integer.parseInt(Entrada.readLine());
     
-    
-        for(int lin=9; lin >= 0; lin--){
-                
-            for (int col=0; col<=lin; col++){
-                System.out.print('.');
-            }
-            for(int col=lin; col<=8; col++){
-                System.out.print(col);
-            }
-            System.out.print(9);
-                
-                for (int col=8; !(col<lin); col--){
-                    System.out.print(col);
+            for(int i= 1; i <= quant; i++){
+                for(int lin=9; lin >= 0; lin--){
+                        
+                    for (int col=0; col<=lin; col++){
+                        System.out.print('.');
+                    }
+                    for(int col=lin; col<=8; col++){
+                        System.out.print(col);
+                    }
+                    System.out.print(9);
+                        
+                        for (int col=8; !(col<lin); col--){
+                            System.out.print(col);
+                        }
+                        for(int col=0; col<=lin; col++){
+                        System.out.print(".");
+                    }
+                        System.out.println();
                 }
-                for(int col=lin; col<=0; col++){
-                System.out.print(col);
             }
-                System.out.println();
-        }
     }
 }
