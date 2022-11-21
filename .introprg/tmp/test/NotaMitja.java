3342cfdfef01f9f8a812905d4ec36f2bbd6cd250
prgtest <cf22angelo.montenegro@iesjoandaustria.org>
2022-11-21 16:30:04 +0100

    correcion ejercicio  ∾ prgtest resultat 31_37: FALLA

---
+++ .introprg/tmp/test/NotaMitja.java
@@ -0,0 +1,33 @@
+/*Programa que da la la media de los valorres ingresado es decir la nota promedio
+*/
+public class NotaMitja {
+    public static void main(String[]args){
+     // creem la variable on guardarem el resultat de sumar
+        double suma = 0;   // inicialment no hem sumat res i per tant és 0
+        double promedio =  0.0  ;
+        double valor = 0;
+        double contador= 1;
+        
+        // demana el primer valor
+        System.out.println("Introdueix un valor");       
+        valor = Double.parseDouble(Entrada.readLine());   
+        
+       if(valor < 0 || valor > 100){
+                System.out.println("Cap nota vàlida introduïda");  
+        }else {
+         suma= suma +valor;
+        // ves sumant mentre el valor sigui positiu
+        while (valor >= 0 && valor <= 100) {
+            System.out.println("Introdueix un valor");
+                valor = Double.parseDouble(Entrada.readLine());
+            if (valor >= 0 && valor <= 100){
+                suma = suma + valor;
+                contador= contador + 1;
+            }
+        }
+        promedio= suma / contador;
+        System.out.println("La mitja de les notes valides es " + promedio);
+         }
+    }
+}
+
---
+++ 31_61_informestring/InformeString.java
@@ -0,0 +1,5 @@
+/*Desarrolla un programa que solicite dos textos y un entero positivo y componga un pequeño informe con el resultado de las funciones anteriores*/
+public class InformeString{
+    public static void main(String[]args){
+    
+    
