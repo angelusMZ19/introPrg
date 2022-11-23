ec9b7f1d6297cedc14778973f56568b3c7ac7410
prgtest <cf22angelo.montenegro@iesjoandaustria.org>
2022-11-23 13:34:29 +0100

    finalizado  ∾ prgtest resultat 31_70: FALLA

---
+++ .introprg/tmp/test/AnalitzaCaracter.java
@@ -0,0 +1,36 @@
+/*Desarrolla un programa que solicite un texto y una posición.
+
+Si la posición no existe dentro del texto, indicará que está fuera de rango.
+
+Si la posición es negativa, empezará a contar a partir del último carácter del texto introducido. Por ejemplo, -1 corresponderá al último carácter.
+*/
+
+public class AnalitzaCaracter{
+    public static void main(String[]args){
+    
+        System.out.println("Text?");
+        String text= Entrada.readLine();
+        
+        System.out.println("Posicio?");
+        int num= Integer.parseInt(Entrada.readLine());
+    
+        if (num > text.length() - 1 || Math.abs(num) > text.length()){
+            System.out.println("Fora de Rang");
+        }   else{
+                if (num < 0){
+                    num = text.length() + num;
+                }
+                char numText = text.charAt(num);
+                if (Character.isLetter(numText)){
+                    String isLetter = String.format("\'%c\' es una lletra", numText);
+                    System.out.println(isLetter);
+                } else if(Character.isDigit(numText)){
+                    String isDigit = String.format("\'%c\' es una nombre", numText);
+                    System.out.println(isDigit);
+                } else if(!Character.isLetter(numText) && !Character.isDigit(numText)){
+                    String altraCosa = String.format("\'%c\' es un altra cosa", numText);
+                    System.out.println(altraCosa);
+                }
+            }
+    }
+}
