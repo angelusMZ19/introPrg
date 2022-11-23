271e2d9843c63d700c0adb15676abb91cfb527f7
prgtest <cf22angelo.montenegro@iesjoandaustria.org>
2022-11-23 10:11:34 +0100

    revision  ∾ prgtest resultat 31_22: PASSA

---
+++ .introprg/tmp/test/MesosAnys.java
@@ -0,0 +1,41 @@
+/*programa que da segun el numero de mes que se introduzca , el numero del mes que sigue y del anteriror con el año al que corresponde
+*/
+public class MesosAnys {
+    public static void main(String[]args){
+    System.out.println("Mes?");
+      int mes = Integer.parseInt(Entrada.readLine());
+    
+    System.out.println("Any?");
+      int any = Integer.parseInt(Entrada.readLine());
+      int mesAnt= mes - 1;
+      int mesDes = mes + 1;
+      int anyAnt = any -1;
+      int anyDes= any + 1;
+      
+        if (mes == 1){
+            System.out.println("Mes anterior "+ "12" +"/"+ anyAnt + " i " + "mes següent " + mesDes + "/" + any );
+            }else if (mes == 2){
+                System.out.println("Mes anterior "+ mesAnt +"/"+ any + " i " + "mes següent " + mesDes + "/" + any );
+                }else if (mes == 3){
+                    System.out.println("Mes anterior "+ mesAnt +"/"+ any + " i " + "mes següent " + mesDes + "/" + any );
+                    }else if (mes == 4){
+                        System.out.println("Mes anterior "+ mesAnt +"/"+ any + " i " + "mes següent " + mesDes + "/" + any );
+                        }else if (mes == 5){
+                            System.out.println("Mes anterior "+ mesAnt +"/"+ any + " i " + "mes següent " + mesDes + "/" + any );
+                            }else if (mes == 6){
+                                System.out.println("Mes anterior "+ mesAnt +"/"+ any + " i " + "mes següent " + mesDes + "/" + any );
+                                }else if (mes == 7){
+                                    System.out.println("Mes anterior "+ mesAnt +"/"+ any + " i " + "mes següent " + mesDes + "/"  + any );
+                                    }else if (mes == 8){
+                                        System.out.println("Mes anterior "+ mesAnt +"/"+ any + " i " +  "mes següent " + mesDes + "/" + any );
+                                        }else if (mes == 9){
+                                            System.out.println("Mes anterior "+ mesAnt +"/"+ any +  " i " + "mes següent " + mesDes + "/" + any );
+                                            }else if (mes == 10){
+                                                System.out.println("Mes anterior "+ mesAnt +"/"+ any + " i " + "mes següent " + mesDes + "/" + any );
+                                                }else if (mes == 11){
+                                                    System.out.println("Mes anterior "+ mesAnt +"/"+ any + " i " + "mes següent " + mesDes + "/" + any );
+                                                    }else if (mes == 12){
+                                                        System.out.println("Mes anterior "+ mesAnt + "/"+ any + " i " + "mes següent " + "1"+ "/"  + anyDes );
+                                                        }
+        }
+}
