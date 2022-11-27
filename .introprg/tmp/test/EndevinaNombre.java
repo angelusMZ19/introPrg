e2b4a6ea2845fd43a823ea52ebd3ae0a2f57db0e
prgtest <cf22angelo.montenegro@iesjoandaustria.org>
2022-11-27 12:37:22 +0100

    cambios que no funcionan  ∾ prgtest resultat 31_03: PASSA

--- .introprg/tmp/test/EndevinaNombre.java
+++ .introprg/tmp/test/EndevinaNombre.java
@@ -1,54 +1,51 @@
-/* olvemos al ejercicio que implementaba el juego de adivinar
-En esta ocasión vamos a desarrollar una nueva versión que mejorará el ejercicio.
-La versión anterior obligaba a los usuarios a acertar para poder finalizar.
-Esta nueva versión ofrece la posibilidad de salir del juego al introducir una cadena vacía (recuerda isEmpty()). Cuando los usuarios entren la cadena vacía, el programa responderá con el mensaje "Cancelado!" y finalizará.*/
+/*
+     * Aquest programa mostra capacitats d'endevinació
+     *
+     * Per executar-ho, fes
+     * $ java EndevinaNombre 42
+     */
 
-public class EndevinaNombre{
-    public static void main (String[]args){
-    
-    int valor = 0;
-    String numCadena= String.valueOf(valor);
-    int numPensado = 42;
-    
-    
-    System.out.println("Ves introduint enters entre 1 i 100 fins que encertis el que jo he pensat");
-    System.out.println("Introdueix un valor");
-    valor = Integer.parseInt(Entrada.readLine());
-    
-    
-       
-        if(!(numCadena.isEmpty()) && !(numCadena.isBlank())){ 
-        
-                
-         while(valor != 42){ 
-         
-         
-             if(valor <= 0){
-             System.out.println("Com a mínim 1");
-                
-                System.out.println("Introdueix un valor");
-                valor = Integer.parseInt(Entrada.readLine());
-            }   else if(valor > 100){
-                    System.out.println("Com a màxim 100");
+    public class EndevinaNombre {
 
-                    System.out.println("Introdueix un valor");
-                    valor = Integer.parseInt(Entrada.readLine());
-                }       else if(valor < numPensado){
-                            System.out.println("És més gran que " + valor);
-                            
-                            System.out.println("Introdueix un valor");
-                            valor = Integer.parseInt(Entrada.readLine());
-                        }   else if(valor > numPensado){
-                                System.out.println("És més petit que " + valor);
-                                
-                                System.out.println("Introdueix un valor");
-                                valor = Integer.parseInt(Entrada.readLine());
-                            }
-                                
-            }
-             System.out.println("Has encertat!");
-         }
-         System.out.println("Cancelat");
+        public static void main(String[] args) {
+
+
+            int numeroPensat= Integer.parseInt(args[0]);   // guardarà el número pensat
+            int numero;         // guardarà els càlculs intermitjos
+
+
+            // pensa un número
+
+            System.out.print("El número pensat és ");
+            System.out.println(numeroPensat);
+
+
+            // Multiplica'l per 3
+
+            numero = numeroPensat * 3;
+            System.out.print("Quan el multipliques per 3 obtens ");
+            System.out.println(numero);
+
+            // Suma-li 6
+
+            numero = numero + 6;
+            System.out.print("Quan li sumes 6 arriba a ");
+            System.out.println(numero);
+
+            // divideix-ho tot per 3gi
+
+            numero = numero / 3;
+            System.out.print("Un cop dividit per 3 queda ");
+            System.out.println(numero);
+
+            // resta-li el número pensat
+            numero = numero - numeroPensat;
+            System.out.print("Finalment, en restar-li el valor inicial, queda ");
+            System.out.println(numero);
+
+            // compara el resultat
+
+            System.out.println("A que el resultat és 2?");
+        }
+
     }
-}
-        
