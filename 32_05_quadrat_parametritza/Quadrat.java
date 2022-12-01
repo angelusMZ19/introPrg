20626657273b210ae5cbc7173db82904d99dfcc7
Angelo Enrique Montenegro Zavala <cf22angelo.montenegro@iesjoandaustria.org>
2022-12-01 17:30:35 +0100

    finalizado 32_05

---
+++ 32_05_quadrat_parametritza/Quadrat.java
@@ -0,0 +1,18 @@
+public class Quadrat {
+    public static void main (String[]args){
+    
+        int numero = Integer.parseInt(args[0]);
+        // dibuixa un quadrat
+            for (int linia=1; linia <= numero; linia++) {
+                // dibuixa una línia
+                for (int columna=1; columna <= numero; columna++) {
+                    // dibuixa un element de la línia
+                    System.out.print(" X");
+                }
+                // dibuixa un salt de línia
+                System.out.println("");
+            }
+            System.out.println("");
+    }
+}
+
