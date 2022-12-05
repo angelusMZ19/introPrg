7bdc2f55a7d1573ff43ad33dae02e621a3774b84
prgtest <cf22angelo.montenegro@iesjoandaustria.org>
2022-12-05 16:54:52 +0100

    finalizado 32_06  ∾ prgtest resultat 32_06: FALLA

--- .introprg/tmp/test/Quadrat.java
+++ .introprg/tmp/test/Quadrat.java
@@ -1,22 +1,26 @@
-/*Programa que imprime la cantidad de columnas y filas dependiendo de la cantidad de numero que se introduzca en el args()
+/*
+En esta ocasión, el programa será capaz de mostrar un carácter diferente de Xa partir de lo que main()reciba enargs[1] - 
+Por simplificar, presupondremos también que siempre nos dan este segundo argumento y que nos quedaremos con el primer carácter.
 */
+
 public class Quadrat {
-    public static void main (String[]args){
-        int numero = Integer.parseInt(args[0]);
-        dibuixaQuadrat(numero);
+    public static void main(String[] args) {
+        int costat = Integer.parseInt(args[0]);
+        char caracter = args[1].charAt(0);
+        dibuixaQuadrat(costat, caracter);
     }
-    public static void dibuixaQuadrat(int numero) {
-        // dibuixa un quadrat
-            for (int linia=1; linia <= numero; linia++) {
-                // dibuixa una línia
-                for (int columna=1; columna <= numero; columna++) {
-                    // dibuixa un element de la línia
-                    System.out.print(" X");
-                }
-                // dibuixa un salt de línia
-                System.out.println("");
-            }
-            System.out.println("");
+
+    public static void dibuixaQuadrat(int costat, char caracter) {
+        for (int linia=1; linia <= costat; linia++) {
+            dibuixaLinia(costat, caracter);
+            System.out.println(" ");
+        }
+    }
+
+    public static void dibuixaLinia(int costat, char caracter) {
+        for (int columna=1; columna <= costat; columna++) { 
+            System.out.print(" " + caracter);
+        }
     }
 }
 
