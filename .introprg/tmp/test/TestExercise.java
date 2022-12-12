1868dee97f1d348640c650a4496bee99d666f75a
prgtest <cf22angelo.montenegro@iesjoandaustria.org>
2022-12-12 17:14:50 +0100

    finalizado 32_04 con capcalera  ∾ prgtest resultat 32_07: PASSA

--- .introprg/tmp/test/TestExercise.java
+++ .introprg/tmp/test/TestExercise.java
@@ -11,10 +11,10 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TestExercise {
 
     @Test
-    @DisplayName("Test hi és mòdul dibuixaQuadrat()")
-    public void prgtestX1dibuixaQuadrat() {
-        Class classe = Quadrat.class;
-        String modulObjectiu = "dibuixaQuadrat";
+    @DisplayName("Test hi és mòdul analitzaCaracter()")
+    public void prgtestX1analitzaCaracter() {
+        Class classe = AnalitzaCaracter.class;
+        String modulObjectiu = "analitzaCaracter";
         Method[] methods = classe.getDeclaredMethods();
         boolean foundTarget = false;
         for (Method method: methods) {
@@ -22,53 +22,13 @@ public class TestExercise {
                 foundTarget=true;
                 Type[] types = method.getGenericParameterTypes();
                 assertEquals(
-                        2, 
+                        1, 
                         types.length, 
                         "Revisa els paràmetres requerits pel mòdul " + modulObjectiu + "()"
                 );
                 assertEquals(
-                    "int",
-                    types[0].getTypeName(),
-                    "Revisa els paràmetres requerits pel mòdul " + modulObjectiu + "()"
-                );
-                assertEquals(
                     "char",
-                    types[1].getTypeName(),
-                    "Revisa els paràmetres requerits pel mòdul " + modulObjectiu + "()"
-                );
-                assertEquals(
-                        "void",
-                        method.getGenericReturnType().getTypeName(),
-                        "S'esperava " + modulObjectiu + "() d'un tipus diferent"
-                );
-            }
-        }
-        assertTrue(foundTarget, "No es troba el mòdul " + modulObjectiu + "(). Revisa enunciat.");
-    }
-    @Test
-    @DisplayName("Test hi és mòdul dibuixaLinia()")
-    public void prgtestX2dibuixaLinia() {
-        Class classe = Quadrat.class;
-        String modulObjectiu = "dibuixaLinia";
-        Method[] methods = classe.getDeclaredMethods();
-        boolean foundTarget = false;
-        for (Method method: methods) {
-            if (modulObjectiu.equals(method.getName())) {
-                foundTarget=true;
-                Type[] types = method.getGenericParameterTypes();
-                assertEquals(
-                        2, 
-                        types.length, 
-                        "Revisa els paràmetres requerits pel mòdul " + modulObjectiu + "()"
-                );
-                assertEquals(
-                    "int",
                     types[0].getTypeName(),
-                    "Revisa els paràmetres requerits pel mòdul " + modulObjectiu + "()"
-                );
-                assertEquals(
-                    "char",
-                    types[1].getTypeName(),
                     "Revisa els paràmetres requerits pel mòdul " + modulObjectiu + "()"
                 );
                 assertEquals(
