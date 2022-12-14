5dd7458908f3245d86ad61815aa12471cd6f4668
Angelo Enrique Montenegro Zavala <cf22angelo.montenegro@iesjoandaustria.org>
2022-12-15 00:59:50 +0100

    finalizado 31_74

--- 31_74_cadena_continua/CadenaContinua.java
+++ 31_74_cadena_continua/CadenaContinua.java
@@ -3,32 +3,36 @@
 public class CadenaContinua{
     public static void main(String[]args){
     
+        int dif= 0;
+        int resultado= 0;
+        
         System.out.println("Text?");
         String text = Entrada.readLine();
-        
+
+        if(!(text.isEmpty())|| !(text.isBlank())){
         System.out.println("Nombre?");
         int n = Integer.parseInt(Entrada.readLine());
-            
-            /*if(n > text.length()){
-                n= text.length();
-            if(n <= text.length()){
-                for (int i = n; i >= text.length(); i++){
-                    System.out.println(text.charAt(i));
+        
+            if(!(n<= 0) && n > text.length()){
+                resultado=  n/text.length();
+                dif= n-(text.length()*resultado);
+                System.out.println(text.repeat(resultado) + text.substring(0, dif));
+            } else if(n==1){
+                n = 0;
+                for (int i= 0; i<= n; i = i +1){
+                    System.out.print(text.charAt(i));
+                }
+            } else if (n >1){
+                for (int i= 0; i<= n-1; i = i +1){
+                    System.out.print(text.charAt(i));
+                }
+            } else{
+                for (int i= 1; i<= n; i = i +1){
+                    System.out.print(text.charAt(i));
                 }
             }
-            if (n > text.length()){
-                for(int i = n; i <= n; i++){
-                    System.out.println(text.charAt(i));
-                }
-            }
-            if (text.isEmpty() && text.isBlank()){
-                System.out.println("Error");
-            }
-        }*/
-        if(n > text.length()){
-            for (int i= text.length(); i<= text.length(); i++){
-                System.out.print(text.charAt(n));
-            }
-         }
+        }else{
+            System.out.println("error");
+        }
     }
 }
