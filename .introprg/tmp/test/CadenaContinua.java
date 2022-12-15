798823fd050f4eb2e371ffa210608d55c2f7f651
prgtest <cf22angelo.montenegro@iesjoandaustria.org>
2022-12-15 01:31:16 +0100

    finalizado 32_09  ∾ prgtest resultat 32_09: FALLA

---
+++ .introprg/tmp/test/CadenaContinua.java
@@ -0,0 +1,45 @@
+/*
+Desenvolupa un programa que demani un text i un nombre enter, i mostri tants caràcters del text com indiqui el nombre enters, començant pel primer. En cas que en faltin, el programa tornarà a mostrar el text a partir del primer caràcter fins que hagi aconseguit tots els caràcters demanats.
+
+En cas que se li demani menys d'un caràcter, simplement no mostrarà res. En cas que el text introduït sigui buit, mostrarà el missatge "error" finalitzarà l'execució sense demanar la longitud de la cadena a generar.
+*/
+public class CadenaContinua{
+    public static void main(String[]args){
+
+        System.out.println("Text?");
+        String text = Entrada.readLine();
+
+        
+        System.out.println("Nombre?");
+        int n = Integer.parseInt(Entrada.readLine());
+
+        mostraCadenaContinua(text, n);
+    }
+    public static void mostraCadenaContinua(String text, int longitud){
+        int dif= 0;
+        int resultado= 0;
+        if(!(text.isEmpty())|| !(text.isBlank())){
+            if(!(longitud<= 0) && longitud > text.length()){
+                resultado =  longitud/text.length();
+                dif= longitud-(text.length() * resultado);
+                System.out.println(text.repeat(resultado) + text.substring(0, dif));
+            } else if(longitud == 1){
+            longitud = 0;
+                for (int i= 0; i<= longitud; i = i +1){
+                    System.out.print(text.charAt(i));
+                }
+            } else if (longitud >1){
+                for (int i= 0; i<= longitud-1; i = i +1){
+                    System.out.print(text.charAt(i));
+                }
+            } else{
+                for (int i= 1; i<= longitud; i = i +1){
+                    System.out.print(text.charAt(i));
+                }
+            }
+        }else{
+           System.out.println("error");
+        }
+    }
+}
+
