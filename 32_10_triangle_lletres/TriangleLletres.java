05999839cde7ee87ac852ebfc0e82249145d50f9
Angelo Enrique Montenegro Zavala <cf22angelo.montenegro@iesjoandaustria.org>
2022-12-15 19:25:01 +0100

    finlizado

---
+++ 32_10_triangle_lletres/TriangleLletres.java
@@ -0,0 +1,6 @@
+/*
+La nova versió tindrà la mateixa sortida que l'original però el codi del programa serà més modular. En concret, el mòdul main() se se n'encarregarà d'obtenir les dades d'entrada, i cridarà un nou mòdul anomenat dibuixaTriangle() que serà qui realitzi realment la feina de mostrar el triangle. De fet, aquest es recolzarà en un altre anomenat dibuixaLinia() que serà qui realment faci la feina de dibuixar cada línia del triangle.
+*/
+public class TriangleLletres{
+    public static void main (String[]args){
+    
