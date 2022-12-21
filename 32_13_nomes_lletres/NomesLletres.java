d6af93c04ce489d535267fdddff74e9736d1e255
Angelo Enrique Montenegro Zavala <cf22angelo.montenegro@iesjoandaustria.org>
2022-12-21 14:05:31 +0100

    finalizado 32_13

---
+++ 32_13_nomes_lletres/NomesLletres.java
@@ -0,0 +1,21 @@
+/*
+La nova versió tindrà la mateixa sortida que l'original però el codi del programa serà més modular. En concret, el mòdul main() se n'encarregarà d'obtenir les dades d'entrada, i cridarà un nou mòdul que serà qui realitzi realment la feina.
+*/
+public class NomesLletres{
+    public static void main (String[]args){
+        
+        System.out.println("Text?");
+        String text= Entrada.readLine();
+        parentitza(text);
+    }
+    public static void parentitza(String text){ 
+        for(int i = 0; i < text.length(); i++){
+            if (Character.isLetter(text.charAt(i))){
+                System.out.print( text.charAt(i) + ",");
+            }
+        }
+        System.out.println();
+    }
+}
+
+
