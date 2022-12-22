982103a36df1667709a267ed6c715c6f15d35fde
prgtest <cf22angelo.montenegro@iesjoandaustria.org>
2022-12-22 14:43:23 +0100

    finalizado 32_13  ∾ prgtest resultat 32_13: FALLA

--- .introprg/tmp/test/NomesLletres.java
+++ .introprg/tmp/test/NomesLletres.java
@@ -10,7 +10,7 @@ public class NomesLletres{
     }
     public static void parentitza(String text){ 
         for(int i = 0; i < text.length(); i++){
-            if (i == text.length() - 2){
+            if (i == text.length() - 1){
                     System.out.print(text.charAt(i) );
                 }else if (Character.isLetter(text.charAt(i))){
                     System.out.print( text.charAt(i) + ", ");
