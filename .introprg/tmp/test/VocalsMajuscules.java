32266884968d2b695c1655dee0fe14add728fed7
prgtest <cf22angelo.montenegro@iesjoandaustria.org>
2022-12-23 19:27:02 +0100

    finalizado 32_14  ∾ prgtest resultat 32_14: PASSA

--- .introprg/tmp/test/VocalsMajuscules.java
+++ .introprg/tmp/test/VocalsMajuscules.java
@@ -1,7 +1,6 @@
 /*
-
+La nova versió tindrà la mateixa sortida que l'original però el codi del programa serà més modular. En concret, el mòdul main() se n'encarregarà d'obtenir les dades d'entrada, i cridarà un nou mòdul que serà qui realitzi realment la feina.
 */
-//Desenvolupa una nova versió de l'exercici que filtrava les lletres d'un text.//
 public class VocalsMajuscules {
     public static void main(String[] args) {
         System.out.println("Text?");
