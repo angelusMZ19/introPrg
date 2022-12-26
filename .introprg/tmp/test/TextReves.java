5b6196b3850d24e6c4ad16828d3203b9cfbf7e34
prgtest <cf22angelo.montenegro@iesjoandaustria.org>
2022-12-26 14:54:50 +0100

    finalizado 31_83  ∾ prgtest resultat 31_83: PASSA

--- .introprg/tmp/test/TextReves.java
+++ .introprg/tmp/test/TextReves.java
@@ -1,15 +1,11 @@
 /*
-La nova versió tindrà la mateixa sortida que l'original però el codi del programa serà més modular. En concret, el mòdul main() se n'encarregarà d'obtenir les dades d'entrada, i cridarà un nou mòdul que serà qui realitzi realment la feina.
 */
 public class TextReves{
     public static void main (String[]args){
     
         System.out.println("Text?");
         String text= Entrada.readLine();
-        
-        mostraReves(text);
-    }
-    public static void mostraReves(String text){
+
         int inverso = text.length()-1;
         String cad= "";
         boolean chequeo= false;
