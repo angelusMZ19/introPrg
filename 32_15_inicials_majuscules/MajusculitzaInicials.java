6803c99382661ca1467623315b1d4af8a7dc4a9d
prgtest <cf22angelo.montenegro@iesjoandaustria.org>
2022-12-27 14:50:59 +0100

    finalizado 32_15  ∾ prgtest autocommit 32_15 #10

--- 32_15_inicials_majuscules/MajusculitzaInicials.java
+++ 32_15_inicials_majuscules/MajusculitzaInicials.java
@@ -1,17 +1,19 @@
 /*
+Desenvolupa una nova versió de l'exercici que posava en majúscules les inicials d'un text.
+La nova versió tindrà la mateixa sortida que l'original però el codi del programa serà més modular. En concret, el mòdul main() se n'encarregarà d'obtenir les dades d'entrada, i cridarà un nou mòdul que serà qui realitzi realment la feina
 */
 public class MajusculitzaInicials{
     public static void main (String[]args){
     
     System.out.println("Text?");
-        String text= Entrada.readLine();
+        String cadena = Entrada.readLine();
 
-        String cadena = text.toLowerCase();
-        char[] caracteres = cadena.toCharArray();
+        String text = cadena.toLowerCase();
+        char[] caracteres = text.toCharArray();
         caracteres[0] = Character.toUpperCase(caracteres[0]);
       
-        for (int i = 0; i < cadena.length()- 1; i++) {
-            if (!(Character.isLetter(cadena.charAt(i)))|| caracteres[i] == '^'){
+        for (int i = 0; i < text.length()- 1; i++) {
+            if (!(Character.isLetter(text.charAt(i)))|| caracteres[i] == '^'){
                 caracteres[i + 1] = Character.toUpperCase(caracteres[i + 1]);
                 System.out.print(caracteres);
             }
