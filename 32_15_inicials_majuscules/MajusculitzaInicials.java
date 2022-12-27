03a4caa594147420b18a8ff2358850d1d7f54002
Angelo Enrique Montenegro Zavala <cf22angelo.montenegro@iesjoandaustria.org>
2022-12-27 12:13:18 +0100

    finalizado 32_15

---
+++ 32_15_inicials_majuscules/MajusculitzaInicials.java
@@ -0,0 +1,38 @@
+/*
+*/
+public class MajusculitzaInicials{
+    public static void main (String[]args){
+    
+    System.out.println("Text?");
+        String cadena= Entrada.readLine();
+
+        /*int inverso = text.length()-1;
+        String cad= "";
+        boolean chequeo= false;
+        for (int i = inverso; i >= 0; i--){
+        if (Character.isLetter(text.charAt(i))){
+              cad = cad + text.charAt(i);
+                if(chequeo == false){
+                    System.out.print(text.charAt(i));
+                    chequeo = true;
+                }else{
+                    System.out.print(", " +text.toUpperCase() );
+                }
+            }else{
+            System.out.print(", " + text.charAt(i));
+        }
+        }
+        System.out.println();*/
+        char[] caracteres = cadena.toCharArray();
+        caracteres[0] = Character.toUpperCase(caracteres[0]);
+        for (int i = 0; i < cadena.length()- 2; i++) 
+        // Es 'palabra'
+        if (caracteres[i] == ' ' || caracteres[i] == '.' || caracteres[i] == ',')
+        // Reemplazamos
+        caracteres[i + 1] = Character.toUpperCase(caracteres[i + 1]);
+        System.out.print(caracteres);
+        System.out.println();
+        return;
+    }
+}
+
