45c6973f03fd19b9b9ea7d81619172662df5d4b9
prgtest <cf22angelo.montenegro@iesjoandaustria.org>
2022-12-27 20:05:36 +0100

    finalizado 31_81  ∾ prgtest resultat 32_15: PASSA

--- .introprg/tmp/test/MajusculitzaInicials.java
+++ .introprg/tmp/test/MajusculitzaInicials.java
@@ -1,20 +1,21 @@
 /*
-Desenvolupa un programa que demani un text i mostri el mateix text amb les inicials de cada paraula en majúscules i la resta en minúscules.
-Una paraula és cada segment del text que només conté lletres que davant i darrere tenen quelcom que no sigui una lletra, o bé són la primera o darrera paraula del text.
+Desenvolupa una nova versió de l'exercici que posava en majúscules les inicials d'un text.
+La nova versió tindrà la mateixa sortida que l'original però el codi del programa serà més modular. En concret, el mòdul main() se n'encarregarà d'obtenir les dades d'entrada, i cridarà un nou mòdul que serà qui realitzi realment la feina
 */
 public class MajusculitzaInicials{
     public static void main (String[]args){
 
-      System.out.println("Text?");
+        System.out.println("Text?");
         String cadena= Entrada.readLine();
-        while(!(cadena.isBlank()) && !(cadena.isEmpty())){
-        String text = cadena.toLowerCase();
-        char[] caracteres = text.toCharArray();
-        caracteres[0] = Character.toUpperCase(caracteres[0]);
-
-        //if(!(caracteres.isBlank()) || !(caracteres.isEmpty())){
-        for (int i = 0; i <text.length()- 1; i++)
-            if (!(Character.isLetter(text.charAt(i)))|| caracteres[i] == '^')
+            majusculitzaInicials(cadena);
+    }
+    public static void majusculitzaInicials(String text){
+        while(!(text.isBlank()) && !(text.isEmpty())){
+                String texto = text.toLowerCase();
+                char[] caracteres = texto.toCharArray();
+                caracteres[0] = Character.toUpperCase(caracteres[0]);
+        for (int i = 0; i <texto.length()- 1; i++)
+            if (!(Character.isLetter(texto.charAt(i)))|| caracteres[i] == '^')
                 caracteres[i + 1] = Character.toUpperCase(caracteres[i + 1]);
                     System.out.print(caracteres);
                     System.out.println();
