02113f04902bbe996bf72f83da08f6a360955c76
prgtest <cf22angelo.montenegro@iesjoandaustria.org>
2022-12-27 19:07:23 +0100

    finalizado 32_15  ∾ prgtest autocommit 32_15 #19

--- 32_15_inicials_majuscules/MajusculitzaInicials.java
+++ 32_15_inicials_majuscules/MajusculitzaInicials.java
@@ -5,22 +5,23 @@ La nova versió tindrà la mateixa sortida que l'original però el codi del prog
 public class MajusculitzaInicials{
     public static void main (String[]args){
     
-    System.out.println("Text?");
+        System.out.println("Text?");
         String cadena= Entrada.readLine();
-
         String text = cadena.toLowerCase();
+        String buit= "";
         char[] caracteres = text.toCharArray();
         caracteres[0] = Character.toUpperCase(caracteres[0]);
 
-        if(!(cadena.isBlank()) || !(cadena.isEmpty())){
+        if(!(text.isBlank()) || !(text.isEmpty())){
+        //if(!(caracteres.isBlank()) || !(caracteres.isEmpty())){
         for (int i = 0; i <text.length()- 1; i++)
             if (!(Character.isLetter(text.charAt(i)))|| caracteres[i] == '^')
                 caracteres[i + 1] = Character.toUpperCase(caracteres[i + 1]);
                     System.out.print(caracteres);
                     System.out.println();
                     return;
-    }else {
-    System.out.println("error");
-    }
+        }else{
+        System.out.print("error");
+        }
     }
 }
