5960c21a481cb6d25788eb79c3fa48206989e527
prgtest <cf22angelo.montenegro@iesjoandaustria.org>
2022-12-27 14:54:27 +0100

    finalizado 32_15  ∾ prgtest resultat 32_15: FALLA

--- .introprg/tmp/test/MajusculitzaInicials.java
+++ .introprg/tmp/test/MajusculitzaInicials.java
@@ -10,15 +10,13 @@ public class MajusculitzaInicials{
         char[] caracteres = cadena.toCharArray();
         caracteres[0] = Character.toUpperCase(caracteres[0]);
       
-        for (int i = 0; i < cadena.length()- 1; i++) 
-
-        if (!(Character.isLetter(cadena.charAt(i)))|| caracteres[i] == '^')
-        caracteres[i + 1] = Character.toUpperCase(caracteres[i + 1]);
-   
-        System.out.print(caracteres);
-        
-        System.out.println();
-        return;
-
+        for (int i = 0; i < cadena.length()- 1; i++){
+            if (!(Character.isLetter(cadena.charAt(i)))|| caracteres[i] == '^'){
+                caracteres[i + 1] = Character.toUpperCase(caracteres[i + 1]);
+                    System.out.print(caracteres);
+                    System.out.println();
+                    return;
+            }
+        }
     }
 }
