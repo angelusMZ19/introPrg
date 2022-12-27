9c9640c2d677776d756d10a8f3e91436ee7d314a
prgtest <cf22angelo.montenegro@iesjoandaustria.org>
2022-12-27 20:04:41 +0100

    finalizado 31_81  ∾ prgtest resultat 31_81: PASSA

--- .introprg/tmp/test/MajusculitzaInicials.java
+++ .introprg/tmp/test/MajusculitzaInicials.java
@@ -5,13 +5,14 @@ Una paraula és cada segment del text que només conté lletres que davant i dar
 public class MajusculitzaInicials{
     public static void main (String[]args){
 
-        System.out.println("Text?");
+      System.out.println("Text?");
         String cadena= Entrada.readLine();
-        
-                String text = cadena.toLowerCase();
-                char[] caracteres = text.toCharArray();
-                caracteres[0] = Character.toUpperCase(caracteres[0]);
-        while(!(text.isBlank()) && !(text.isEmpty())){
+        while(!(cadena.isBlank()) && !(cadena.isEmpty())){
+        String text = cadena.toLowerCase();
+        char[] caracteres = text.toCharArray();
+        caracteres[0] = Character.toUpperCase(caracteres[0]);
+
+        //if(!(caracteres.isBlank()) || !(caracteres.isEmpty())){
         for (int i = 0; i <text.length()- 1; i++)
             if (!(Character.isLetter(text.charAt(i)))|| caracteres[i] == '^')
                 caracteres[i + 1] = Character.toUpperCase(caracteres[i + 1]);
