dc3e968bbf0c96429a104ada5b22f9983e82a1fd
prgtest <cf22angelo.montenegro@iesjoandaustria.org>
2022-12-27 13:59:49 +0100

    finalizado 32_15  ∾ prgtest autocommit 32_15 #8

--- 32_15_inicials_majuscules/MajusculitzaInicials.java
+++ 32_15_inicials_majuscules/MajusculitzaInicials.java
@@ -6,41 +6,17 @@ public class MajusculitzaInicials{
     System.out.println("Text?");
         String text= Entrada.readLine();
 
-        /*int inverso = text.length()-1;
-        String cad= "";
-        boolean chequeo= false;
-        for (int i = inverso; i >= 0; i--){
-        if (Character.isLetter(text.charAt(i))){
-              cad = cad + text.charAt(i);
-                if(chequeo == false){
-                    System.out.print(text.charAt(i));
-                    chequeo = true;
-                }else{
-                    System.out.print(", " +text.toUpperCase() );
-                }
-            }else{
-            System.out.print(", " + text.charAt(i));
-        }
-        }
-        System.out.println();*/
         String cadena = text.toLowerCase();
         char[] caracteres = cadena.toCharArray();
         caracteres[0] = Character.toUpperCase(caracteres[0]);
       
-        for (int i = 0; i < cadena.length()- 1; i++) 
-        // Es 'palabra'
-        if (!(Character.isLetter(cadena.charAt(i)))|| caracteres[i] == '^')
-        //if (Character.isLetter(cadena.charAt(i)))
-        // Reemplazamos
-        caracteres[i + 1] = Character.toUpperCase(caracteres[i + 1]);
-   
-        System.out.print(caracteres);
-        
+        for (int i = 0; i < cadena.length()- 1; i++) {
+            if (!(Character.isLetter(cadena.charAt(i)))|| caracteres[i] == '^'){
+                caracteres[i + 1] = Character.toUpperCase(caracteres[i + 1]);
+                System.out.print(caracteres);
+            }
+        }
         System.out.println();
         return;
-
     }
 }
-
-/*(caracteres[i] == ' ' && caracteres[i] == '^'|| caracteres[i] == '.' || caracteres[i] == ','|| caracteres[i] == '-'|| caracteres[i] == '!'|| caracteres[i] == '&'|| caracteres[i] == '%'|| caracteres[i] == '$')
-*/
