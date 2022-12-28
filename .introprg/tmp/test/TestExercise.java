3be9346930ebc7048723feb4745194d0dc2d5bb1
prgtest <cf22angelo.montenegro@iesjoandaustria.org>
2022-12-28 18:28:18 +0100

    finalizado  ∾ prgtest resultat 32_18: FALLA

--- .introprg/tmp/test/TestExercise.java
+++ .introprg/tmp/test/TestExercise.java
@@ -11,10 +11,20 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TestExercise {
 
     @Test
-    @DisplayName("Test hi és mòdul majusculitzaInicials()")
-    public void prgtestX1majusculitzaInicials() {
-        Class classe = MajusculitzaInicials.class;
-        String modulObjectiu = "majusculitzaInicials";
+    @DisplayName("Test no hi és mòdul reemplaçat")
+    public void comparaAiEs() {
+        Method[] methods = ComptaAiEs.class.getDeclaredMethods();
+        boolean foundTarget = false;
+        for (Method method: methods) {
+            assertNotEquals("comparaAiEs", method.getName(), "cal eliminar el mòdul reemplaçat");
+        }
+    }
+
+    @Test
+    @DisplayName("Test hi és mòdul comptaLletra()")
+    public void prgtestX1comptaLletra() {
+        Class classe = ComptaAiEs.class;
+        String modulObjectiu = "comptaLletra";
         Method[] methods = classe.getDeclaredMethods();
         boolean foundTarget = false;
         for (Method method: methods) {
@@ -22,13 +32,58 @@ public class TestExercise {
                 foundTarget=true;
                 Type[] types = method.getGenericParameterTypes();
                 assertEquals(
-                        1, 
+                        2, 
                         types.length, 
                         "Revisa els paràmetres requerits pel mòdul " + modulObjectiu + "()"
                 );
                 assertEquals(
                     "java.lang.String",
                     types[0].getTypeName(),
+                    "Revisa els paràmetres requerits pel mòdul " + modulObjectiu + "()"
+                );
+                assertEquals(
+                    "char",
+                    types[1].getTypeName(),
+                    "Revisa els paràmetres requerits pel mòdul " + modulObjectiu + "()"
+                );
+                assertEquals(
+                        "void",
+                        method.getGenericReturnType().getTypeName(),
+                        "S'esperava " + modulObjectiu + "() d'un tipus diferent"
+                );
+            }
+        }
+        assertTrue(foundTarget, "No es troba el mòdul " + modulObjectiu + "(). Revisa enunciat.");
+    }
+    @Test
+    @DisplayName("Test hi és mòdul comparaOcurrencies()")
+    public void prgtestX2comparaOcurrencies() {
+        Class classe = ComptaAiEs.class;
+        String modulObjectiu = "comparaOcurrencies";
+        Method[] methods = classe.getDeclaredMethods();
+        boolean foundTarget = false;
+        for (Method method: methods) {
+            if (modulObjectiu.equals(method.getName())) {
+                foundTarget=true;
+                Type[] types = method.getGenericParameterTypes();
+                assertEquals(
+                        3, 
+                        types.length, 
+                        "Revisa els paràmetres requerits pel mòdul " + modulObjectiu + "()"
+                );
+                assertEquals(
+                    "java.lang.String",
+                    types[0].getTypeName(),
+                    "Revisa els paràmetres requerits pel mòdul " + modulObjectiu + "()"
+                );
+                assertEquals(
+                    "char",
+                    types[1].getTypeName(),
+                    "Revisa els paràmetres requerits pel mòdul " + modulObjectiu + "()"
+                );
+                assertEquals(
+                    "char",
+                    types[2].getTypeName(),
                     "Revisa els paràmetres requerits pel mòdul " + modulObjectiu + "()"
                 );
                 assertEquals(
