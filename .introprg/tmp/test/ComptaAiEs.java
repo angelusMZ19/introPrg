1c52e7e80ff4f71389a5c75b7bdc267087d30c58
prgtest <cf22angelo.montenegro@iesjoandaustria.org>
2022-12-29 14:43:15 +0100

    finalizado  ∾ prgtest resultat 32_18: FALLA

--- .introprg/tmp/test/ComptaAiEs.java
+++ .introprg/tmp/test/ComptaAiEs.java
@@ -78,7 +78,7 @@
 }*/
 
 /* programa que mostra quantes 'a's i 'e's té el text llegit per stdin */
-public class ComptaAiEs {
+/*public class ComptaAiEs {
     public static void main(String[] args) {
         System.out.println("Introdueix un text");
         String entrada = Entrada.readLine();
@@ -118,7 +118,7 @@ public class ComptaAiEs {
             System.out.println("Hi ha tantes 'a's com 'e's");
         }
     }
-}
+}*/
 
 
 /*
@@ -164,4 +164,50 @@ public class ComptaAiEs {
 }
 
 */
+
+
+
+/* programa que mostra quantes 'a's i 'e's té el text llegit per stdin */
+public class ComptaAiEs {
+    public static void main(String[] args) {
+        System.out.println("Introdueix un text");
+        String entrada = Entrada.readLine();
+        char lletra1 = 'a';
+        char lletra2 = 'e';
+        comptaLletra(entrada, 'a');
+        comptaLletra(entrada, 'e');
+        
+        comparaOcurrencies(entrada, lletra1, lletra2);
+    }
+    public static void comptaLletra(String text, char lletra){
+        int comptadorlletra = 0;
+        for (int i=0; i < text.length(); i++) {
+            if (text.charAt(i) == lletra) {
+                comptadorlletra += 1;
+            /*}else if (text.charAt(i) == lletra2){
+                comptadorlletra2 += 1;
+            }*/
+        }
+        System.out.println("Nombre de '" + lletra + "'s: " + comptadorlletra);
+        }
+    }
+    public static void comparaOcurrencies(String text, char lletra1, char lletra2) {
+        int comptador1 = 0;
+        int comptador2 = 0;
+        for (int i=0; i < text.length(); i++) {
+            if (text.charAt(i) == lletra1) {
+                comptador1 += 1;
+            } else if (text.charAt(i) == lletra2) {
+                comptador2 += 1;
+            }
+        }
+        if (comptador1 > comptador2) {
+            System.out.println("Hi ha més 'a's que 'e's");
+        } else if (comptador1 < comptador2) {
+            System.out.println("Hi ha menys 'a's que 'e's");
+        } else {
+            System.out.println("Hi ha tantes 'a's com 'e's");
+        }
+    }
+}
 
