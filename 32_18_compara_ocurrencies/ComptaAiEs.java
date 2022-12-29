e3fc6c578bb0130031651a0b183488a3eb292e9c
prgtest <cf22angelo.montenegro@iesjoandaustria.org>
2022-12-29 14:34:20 +0100

    finalizado  ∾ prgtest autocommit 32_18 #7

--- 32_18_compara_ocurrencies/ComptaAiEs.java
+++ 32_18_compara_ocurrencies/ComptaAiEs.java
@@ -78,7 +78,7 @@
 }*/
 
 /* programa que mostra quantes 'a's i 'e's té el text llegit per stdin */
-public class ComptaAiEs {
+/*public class ComptaAiEs {
     public static void main(String[] args) {
         System.out.println("Introdueix un text");
         String entrada = Entrada.readLine();
@@ -118,5 +118,49 @@ public class ComptaAiEs {
             System.out.println("Hi ha tantes 'a's com 'e's");
         }
     }
+}*/
+
+
+
+public class ComptaAiEs {
+    public static void main(String[] args) {
+        System.out.println("Introdueix un text");
+        String entrada = Entrada.readLine();
+        char lletra1 = 'a';
+        char lletra2 = 'e';
+        comparaOcurrencies(entrada, lletra1, lletra2);
+        
+        int comptadorlletra1 = 0;
+        int comptadorlletra2 = 0;
+        for (int i=0; i < entrada.length(); i++) {
+            if (entrada.charAt(i) == lletra1) {
+                comptadorlletra1 += 1;
+            }else if (entrada.charAt(i) == lletra2){
+                comptadorlletra2 += 1;
+            }
+        }
+        System.out.println("Nombre de '" + lletra1 + "'s: " + comptadorlletra1);
+        System.out.println("Nombre de '" + lletra2 + "'s: " + comptadorlletra2);
+    }
+        public static void comparaOcurrencies(String text, char lletra1, char lletra2) {
+        int comptador1 = 0;
+        int comptador2 = 0;
+        for (int i=0; i < text.length(); i++) {
+            if (text.charAt(i) == lletra1) {
+                comptador1 += 1;
+            } else if (text.charAt(i) == lletra2) {
+                comptador2 += 1;
+            }
+        }
+        if (comptador1 > comptador2) {
+            System.out.println("Hi ha més 'a's que 'e's");
+        } else if (comptador1 < comptador2) {
+            System.out.println("Hi ha menys 'a's que 'e's");
+        } else {
+            System.out.println("Hi ha tantes 'a's com 'e's");
+        }
+    }
 }
+
+
 
