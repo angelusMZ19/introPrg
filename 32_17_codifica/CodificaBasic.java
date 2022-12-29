20ee3a1da69510a915d80b705c3964f879d50a1a
Angelo Enrique Montenegro Zavala <cf22angelo.montenegro@iesjoandaustria.org>
2022-12-29 17:18:00 +0100

    test ejercicio

---
+++ 32_17_codifica/CodificaBasic.java
@@ -0,0 +1,11 @@
+/*
+*/
+public class CodificaBasic{
+    public static void main (String[]args){
+    System.out.println("Text?");
+    String text= Entrada.readLine();
+    char abecedario= 'abcdefghijklmnopqrstuvwz';
+    
+    for(int i= 0 ; i <= text.length(); i++){
+     char a= b
+        
