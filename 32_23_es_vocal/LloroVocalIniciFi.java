9ae1305cb7d20eb3c4c2c0c0634bb4fe22b6e4a7
Angelo Enrique Montenegro Zavala <cf22angelo.montenegro@iesjoandaustria.org>
2023-01-02 18:05:37 +0100

    finalizado 32_24

---
+++ 32_23_es_vocal/LloroVocalIniciFi.java
@@ -0,0 +1,7 @@
+/*
+En diferents exercicis ens hem trobat la necessitat de distingir si un caràcter és o no una vocal. Ara que ja coneixem les funcions, podem definir la nostra funció esVocal() que rebi un caràcter i retorni un booleà indicant si el caràcter correspon o no amb una vocal.
+Considerarem com a vocals totes les variants d'accents i dièresis de les vocals catalanes.
+Recorda que aquests variants són, a banda de les cinc habituals, à, è, é, í, ï, ò, ó, ú i ü.
+*/
+public class LloroVocalIniciFi{
+public static void main (String[]args){
---
+++ 32_24_nomes_lletres/NomesLletres.java
@@ -0,0 +1,16 @@
+/*
+Programa de manera modular que muestra solo las letras del el texto que se ha introducido.
+ */
+public class NomesLletres{
+
+    public static void main(String[] args) {
+        System.out.println("Text?");
+        System.out.println(
+            UtilString.lletresSeparades(
+                UtilString.nomesLletres(
+                    Entrada.readLine()
+                    )
+                )
+            );
+    }
+}
