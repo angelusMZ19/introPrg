99226de8d09437b35e5f922ca36a2865f4275fba
prgtest <cf22angelo.montenegro@iesjoandaustria.org>
2023-01-05 17:34:25 +0100

    intento de pasar prgtest  ∾ prgtest resultat 32_30: FALLA

---
+++ .introprg/tmp/test/NotaMesAlta.java
@@ -0,0 +1,22 @@
+/*Desenvolupa un programa que vagi demanant enters positius i que, en el moment que rebi un de negatiu, mostri el valor màxim positiu introduït i finalitzi l'execució.
+*/
+public class NotaMesAlta{
+    public static void main(String[] args) {
+        int valor = 0; 
+        int maxim = 0;
+        System.out.println("Introdueix un valor (-1 per finalitzar)");
+        while (valor >= 0 && valor <=10) {
+            int comptador= 0;
+            valor = Integer.parseInt(Entrada.readLine());
+             if (valor > maxim) {
+                maxim = valor;
+            }if(comptador == 2){
+                System.out.println("Com a minim cales dues notes");
+            } else{
+                comptador += 1;
+            }
+        }
+        System.out.println("La nota més alta és " + maxim);
+    }
+}
+
