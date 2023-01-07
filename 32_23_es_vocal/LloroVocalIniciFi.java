d454db135b0c8e6dfc51650d54575695009d1844
Angelo Enrique Montenegro Zavala <cf22angelo.montenegro@iesjoandaustria.org>
2023-01-07 12:41:51 +0100

    finalizado 32_23

--- 32_23_es_vocal/LloroVocalIniciFi.java
+++ 32_23_es_vocal/LloroVocalIniciFi.java
@@ -2,28 +2,37 @@
 En diferents exercicis ens hem trobat la necessitat de distingir si un caràcter és o no una vocal. Ara que ja coneixem les funcions, podem definir la nostra funció esVocal() que rebi un caràcter i retorni un booleà indicant si el caràcter correspon o no amb una vocal.
 Considerarem com a vocals totes les variants d'accents i dièresis de les vocals catalanes.
 Recorda que aquests variants són, a banda de les cinc habituals, à, è, é, í, ï, ò, ó, ú i ü.
-*//*
+*/
 public class LloroVocalIniciFi{
     public static void main (String[]args){
+        String paraula = "hola";
+        boolean confirmacion = false;
+            while (!confirmacion) {
+            boolean esVocalInici= true;
+            boolean esVocalFin= true;
 
-       String paraula = "hola";
+                System.out.println("El lloro demana paraula amb vocal a l'inici o/i final");
+                    paraula= Entrada.readLine();
 
-        while (!(paraula.isEmpty()) && !(paraula.isBlank())) {
-        System.out.println("El lloro demana paraula amb vocal a l'inici o/i final");
-        paraula= Entrada.readLine();
+                    if(!paraula.isBlank()){
+                        char caracterInici= paraula.charAt(0);
+                        esVocalInici= UtilString.esVocal(caracterInici);
 
-           if(!(paraula.isEmpty()) && !(paraula.isBlank())){
-            System.out.println("El lloro repeteix: " + paraula);
-
-           }
-        }
-     System.out.println ("Adéu");
-    }
-}*/
-
-public class LloroVocalIniciFi{
-    public static void mian (String[]args){
-    System.out.println("El lloro demana paraula amb vocal a l'inici o/i final");
-        System.out.println(UtilString.esVocal(Entrada.readLine()));
+                        char caracterFin= paraula.charAt(paraula.length()-1);
+                        esVocalFin= UtilString.esVocal(caracterFin);
+                        
+                        if(esVocalInici || esVocalFin){
+                            System.out.println("El lloro diu: " + paraula);
+                            }
+                            }else{
+                                System.out.println("El lloro demana confirmacion per finalitzar");
+                                if (confirmacion = UtilitatsConfirmacio.respostaABoolean(Entrada.readLine())){
+                                    confirmacion= true;
+                                } else { 
+                                    confirmacion= false;
+                            }
+                        }
+                    }
+            System.out.println ("Adéu");
     }
 }
--- 32_23_es_vocal/UtilString.java
+++ 32_23_es_vocal/UtilString.java
@@ -1,91 +1,21 @@
 /*
-progrmaa que es usado 
-*//*
-public class UtilString{
-    public static String nomesLletres(String text){
-        String nomesLletres = "";
-        for(int i = 0; i < text.length(); i++){
-            if(Character.isLetter(text.charAt(i))){
-                nomesLletres = nomesLletres + text.charAt(i);
-            }
-        }
-        return nomesLletres;
-    }
-    public static String lletresSeparades(String nomesLletres){
-        String lletresSeparades= "";
-        for(int i = 0; i < nomesLletres.length(); i++){
-            if(nomesLletres.length() == 1){
-                lletresSeparades = lletresSeparades + nomesLletres.charAt(i);
-            } else {
-                    if (i== nomesLletres.length() - 1){
-                        lletresSeparades = lletresSeparades + nomesLletres.charAt(i);
-                    } else {
-                        lletresSeparades = lletresSeparades + nomesLletres.charAt(i) + ", ";
-                }
-            }
-        }
-        return lletresSeparades;
-    }
-}
+util String 
+*te da como resultado si empieza con vocal 
+* o termina con vocal 
 */
-/*
 public class UtilString{
-    public static void esVocal(){
-
-        String text = Entrada.readLine();
-        char inici= text.charAt(0);
-        char fin = text.charAt(text.length()-1);
-        for(int i= 0 ; i <= text.length(); i++){
-            if(inici== 'a'||inici== 'e'||
-               inici== 'i'||inici== 'o'||
-               inici== 'u'||inici== 'A'||
-               inici== 'E'||inici== 'I'||
-               inici== 'O'||inici== 'U'){
-            return esVocal;
-            }else{
-                System.out.println("Error");
-            }
-        }
-    }
-}
-*/
-
-
-public class UtilString{
-    public static void esVocal(String text){
-
-        while(text.length() >= 1){
-            if(text.charAt(0)== 'a'||text.charAt(0)== 'e'||
-               text.charAt(0)== 'i'||text.charAt(0)== 'o'||
-               text.charAt(0)== 'u'||text.charAt(0)== 'A'||
-               text.charAt(0)== 'E'||text.charAt(0)== 'I'||
-               text.charAt(0)== 'O'||text.charAt(0)== 'U'||
-               text.charAt(text.length()-1)== 'a'||text.charAt(text.length()-1)== 'e'||
-               text.charAt(text.length()-1)== 'i'||text.charAt(text.length()-1)== 'o'||
-               text.charAt(text.length()-1)== 'u'||text.charAt(text.length()-1)== 'A'||
-               text.charAt(text.length()-1)== 'E'||text.charAt(text.length()-1)== 'I'||
-               text.charAt(text.length()-1)== 'O'||text.charAt(text.length()-1)== 'U'){
-               
-               System.out.println("el lloro diu: " + text);
-            }
-                System.out.println("El lloro demana paraula amb vocal a l'inici o/i final");
-                text = Entrada.readLine();
-        }
-            System.out.println("Confirmacion");
-            if(UtilitatsConfirmacio.respostaABoolean(Entrada.readLine())){
-                System.out.println("Adeu");
-                System.exit(0);
-            }else{
-                LloroVocalIniciFi();
+    public static boolean esVocal(char caracter){
+    String vocales= "àaèéeíïiòóoúüuÀAÈÉEÍÏIÒÓOÚÜU";
+    boolean esVocal= false;
+    for (int i =0; i< vocales.length(); i++){
+        char vocal= vocales.charAt(i);
+            if (vocal== caracter){
+                esVocal = true;
+                break;
             }
     }
+    return esVocal;
+    }
 }
-
-
-
-
-
-
-
 
 
