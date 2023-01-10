dfffb12940d442948e468eeff5f8e885d032b364
prgtest <cf22angelo.montenegro@iesjoandaustria.org>
2023-01-10 20:15:38 +0100

    finalizado el ejercicio, en este caso para introducir valores desde la linea de comando no se requiere ingresar el args[0 ] si no que mas bien al declarar el String[]args ya esta realizado para ir desde la liena 0  ∾ prgtest resultat 32_38: FALLA

---
+++ .introprg/tmp/test/Arguments.java
@@ -0,0 +1,22 @@
+/*
+En aquest exercici consisteix en fer un programa que analitzi els arguments que es passen per la línia de comandes i distingeixi si són o no enters.
+*/
+public class Arguments{
+    public static void main (String[]args){
+        
+        
+        if (args.length > 0){
+            for (int i = 0; i < args.length; i++) {
+                String text= args[i];
+                boolean numero= UtilString.esEnter(text);
+                if(numero== true){
+                    System.out.println("[" + i + " ]: " + "és enter");
+                }else{
+                    System.out.println("[" + i + " ]: " + "no és enter");
+                }
+            }
+        }else {
+            System.out.println("Cap argument");
+        }
+    }
+}
