091dabcf760ae80b80aa9e21a9801bcbba018529
prgtest <cf22angelo.montenegro@iesjoandaustria.org>
2023-01-11 20:40:30 +0100

    finalizado 32_41_ aplicando los modulos en arrays para proceder a realizar una separaciond el codigo para que se lea mejor  ∾ prgtest resultat 32_41: PASSA

--- .introprg/tmp/test/TresEnRatlla.java
+++ .introprg/tmp/test/TresEnRatlla.java
@@ -1,62 +1,58 @@
 /*
-La representació del joc es realitzarà tenint present que:
+En aquesta ocasió representarem tot el taulell, no amb variables separades fila0, fila1 i fila2, sinó amb una taula de caràcters.
 
-el taulell estarà implementat amb tres arrays de caràcters que representaran cadascuna de les files
-
-cada posició es codificarà amb un de tres possibles caràcters (X, O o ·) El punt indicarà que la posició encara no està marcada.
+se debera realizar de tal forma que el ejercicio pida donde se deberan ubicar los caracteres de O y X por lo tanto manso a la obra 
 */
-public class TresEnRatlla{
-    public static void main(String[]args){
-        //declaració de les files
-        char[] fila0 = new char[3];
-        char[] fila1 = new char[3];
-        char[] fila2 = new char[3];
 
-        //inicialització de la fila 0
-        fila0[0] = 'O';
-        fila0[1] = '·';
-        fila0[2] = 'X';
+public class TresEnRatlla {
+    public static void main(String[] args){
+        // declaració del taulell
+        char[][] taulell = new char[3][3];
 
-        //inicialització de la fila 1
-        fila1[0] = 'X';
-        fila1[1] = 'X';
-        fila1[2] = 'O';
+        // inicialització de la fila 0
+        taulell[0][0] = '·';
+        taulell[0][1] = 'O';
+        taulell[0][2] = 'O';
+        // inicialització de la resta de files
+        taulell[1][0] = '·';
+        taulell[1][1] = 'X';
+        taulell[1][2] = '·';
+        // altres
+        taulell[2][0] = '·';
+        taulell[2][1] = '·';
+        taulell[2][2] = 'X';
 
-        // inicialització de la fila 2
-        fila2[0] = 'O';
-        fila2[1] = '·';
-        fila2[2] = '·';
-        // mostra el taulell
-        mostraFila(fila0);
-        mostraFila(fila1);
-        mostraFila(fila2);
+        // mostra la posició inicial del taulell
+        System.out.println("La posició inicial del taulell:");
+        mostraFila(taulell[0]);     // mostra la línia 0
+        mostraFila(taulell[1]);     // mostra la línia 1
+        mostraFila(taulell[2]);     // mostra la línia 2
+
+        // demana coordenades del moviment del jugador X
+        System.out.println("Fila del següent moviment?");
+        int fila = Integer.parseInt(Entrada.readLine());
+        System.out.println("Columna del següent moviment?");
+        int columna = Integer.parseInt(Entrada.readLine());
+
+        // marquem el nou moviment
+        taulell[fila][columna] = 'X';
+
+        // tornem a mostrar el taulell
+        System.out.println("La posició final del taulell:");
+        mostraFila(taulell[0]);     // mostra la línia 0
+        mostraFila(taulell[1]);     // mostra la línia 1
+        mostraFila(taulell[2]);     // mostra la línia 2
+        
     }
 
     // mostra el contingut de la fila per sortida estàndard
     public static void mostraFila(char[] fila) {
-        for (int i=0; i < 3; i++) {
-            System.out.print(fila[i]);
+        for (int col = 0; col < 3; col++) {
+            System.out.print(fila[col]);
         }
         System.out.println();
     }
 }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-/*
-        char[] top= {'O', '·', 'X'};
-        char[] mid= {'X', 'X', 'O'};
-        char[] bot= {'O', '·', '·'};
-        System.out.println(top);
-        System.out.println(mid);
-        System.out.println(bot);
-    }
-}
-*/
+
+
+
