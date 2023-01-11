3396bdf5719e6bc3bcb4df3c5a6589fb2778ee7f
Angelo Enrique Montenegro Zavala <cf22angelo.montenegro@iesjoandaustria.org>
2023-01-11 19:44:01 +0100

    prova

--- 32_39_nomes_enters/SumaEnters.java
+++ 32_39_nomes_enters/SumaEnters.java
@@ -2,11 +2,19 @@
 */
 public class SumaEnters{
     public static void main (String[]args){
-
-        int i;
-        for (i = 0; i < args.length; i++) {
-            System.out.println(args[i]+ " ");
+        
+        if (args.length > 0){
+            for (int i = 0; i < args.length; i++) {
+                String text= args[i];
+                boolean numero= UtilString.esEnter(text);
+                if(numero== true){
+                    System.out.println("[" + i + "] "+ "\""+text +"\""+ ": " + "és enter"); 
+                }else{
+                    System.out.println("[" + i + "] "+ "\""+text +"\""+ ": " + "no és enter");
+                }
+            }
+        }else {
+            System.out.println("Cap argument");
         }
-        System.out.println();
     }
 }
--- 32_40_tresenratlla_files/TresEnRatlla.java
+++ 32_40_tresenratlla_files/TresEnRatlla.java
@@ -3,9 +3,9 @@
 public class TresEnRatlla{
     public static void main(String[]args){
         
-        char[] top= {o, ., x};
-        char[] mid= {x, x, o};
-        char[] bot= {o, ., .};
+        char[] top= {'o', '.', 'x'};
+        char[] mid= {'x', 'x', 'o'};
+        char[] bot= {'o', '.', '.'};
         System.out.println(top);
         System.out.println(mid);
         System.out.println(bot);
