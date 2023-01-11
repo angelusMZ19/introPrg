480da459d44fa9429fe5094c4b94d91c40a12918
prgtest <cf22angelo.montenegro@iesjoandaustria.org>
2023-01-11 20:05:15 +0100

    finalizado 32_40 ; nota: al ingresar valores a un array de caracteres es decir un char[] se debe agregar a lso valores propuestos las comillas simples de este modo se considerarian valores apropiadso para el tipo de array que se ha realizado  ∾ prgtest resultat 32_40: PASSA

--- .introprg/tmp/test/TresEnRatlla.java
+++ .introprg/tmp/test/TresEnRatlla.java
@@ -1,8 +1,56 @@
 /*
+La representació del joc es realitzarà tenint present que:
+
+el taulell estarà implementat amb tres arrays de caràcters que representaran cadascuna de les files
+
+cada posició es codificarà amb un de tres possibles caràcters (X, O o ·) El punt indicarà que la posició encara no està marcada.
 */
 public class TresEnRatlla{
     public static void main(String[]args){
-        
+        //declaració de les files
+        char[] fila0 = new char[3];
+        char[] fila1 = new char[3];
+        char[] fila2 = new char[3];
+
+        //inicialització de la fila 0
+        fila0[0] = 'O';
+        fila0[1] = '·';
+        fila0[2] = 'X';
+
+        //inicialització de la fila 1
+        fila1[0] = 'X';
+        fila1[1] = 'X';
+        fila1[2] = 'O';
+
+        // inicialització de la fila 2
+        fila2[0] = 'O';
+        fila2[1] = '·';
+        fila2[2] = '·';
+        // mostra el taulell
+        mostraFila(fila0);
+        mostraFila(fila1);
+        mostraFila(fila2);
+    }
+
+    // mostra el contingut de la fila per sortida estàndard
+    public static void mostraFila(char[] fila) {
+        for (int i=0; i < 3; i++) {
+            System.out.print(fila[i]);
+        }
+        System.out.println();
+    }
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+/*
         char[] top= {'O', '·', 'X'};
         char[] mid= {'X', 'X', 'O'};
         char[] bot= {'O', '·', '·'};
@@ -11,3 +59,4 @@ public class TresEnRatlla{
         System.out.println(bot);
     }
 }
+*/
