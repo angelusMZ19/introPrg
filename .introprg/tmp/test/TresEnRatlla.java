49d83c8932d17176ecf0776b6a1911c58a4cb297
prgtest <cf22angelo.montenegro@iesjoandaustria.org>
2023-01-11 19:44:06 +0100

    prova  ∾ prgtest resultat 32_40: FALLA

---
+++ .introprg/tmp/test/TresEnRatlla.java
@@ -0,0 +1,13 @@
+/*
+*/
+public class TresEnRatlla{
+    public static void main(String[]args){
+        
+        char[] top= {'o', '.', 'x'};
+        char[] mid= {'x', 'x', 'o'};
+        char[] bot= {'o', '.', '.'};
+        System.out.println(top);
+        System.out.println(mid);
+        System.out.println(bot);
+    }
+}
