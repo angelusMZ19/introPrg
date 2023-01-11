9231ee23779397e040c41f86167cef48a8457ef6
prgtest <cf22angelo.montenegro@iesjoandaustria.org>
2023-01-11 19:45:53 +0100

    prova  ∾ prgtest resultat 32_40: PASSA

--- .introprg/tmp/test/TresEnRatlla.java
+++ .introprg/tmp/test/TresEnRatlla.java
@@ -3,9 +3,9 @@
 public class TresEnRatlla{
     public static void main(String[]args){
         
-        char[] top= {'O', '.', 'X'};
+        char[] top= {'O', '·', 'X'};
         char[] mid= {'X', 'X', 'O'};
-        char[] bot= {'O', '.', '.'};
+        char[] bot= {'O', '·', '·'};
         System.out.println(top);
         System.out.println(mid);
         System.out.println(bot);
