42662bd37bea5c3742fcea51ae3a2e64cc96f4fd
prgtest <cf22angelo.montenegro@iesjoandaustria.org>
2023-01-16 18:37:10 +0100

    finalizado 32_26 ejercicio realizado con modulos en el que se llama a la funcion string que devuelve un pedazo de string hacia el documento mostraInterval  ∾ prgtest resultat 32_26: FALLA

---
+++ .introprg/tmp/test/UtilString.java
@@ -0,0 +1,30 @@
+/**/
+public class UtilString{
+    public static String intervalString (String text, int inici, int fin ){
+        String buit= "";
+        
+        if (inici < 0){
+            inici = 0;
+        }
+        if (fin < 0){
+            fin = 0;
+        }
+        if (fin > text.length()-1){
+            fin = text.length()-1;
+        }
+        if (inici > text.length()-1){
+            inici = text.length()-1;
+        }
+            if (inici < fin){
+                for (int i= inici; i <= fin; i++){
+                    buit= buit + text.charAt(i);
+                }
+            }else {
+                for(int i= inici; i >= fin; i--){
+                    buit= buit + text.charAt(i);
+                }
+                
+            }
+            return buit;
+    }
+}
