45616edcec877d8d869279c5d568898d98893f20
Angelo Enrique Montenegro Zavala <cf22angelo.montenegro@iesjoandaustria.org>
2023-01-16 16:51:06 +0100

    finalizado 32_34_ en este ejercicio los array son declarado y dados valores

--- 32_42_tresenratlla/TresEnRatlla.java
+++ 32_42_tresenratlla/TresEnRatlla.java
@@ -7,34 +7,93 @@ que comiece el juego y que la suerte este de vuestro lado
 
 public class TresEnRatlla {
     public static void main(String[] args){
+        boolean joc= true;
         
-         // declaració del taulell
-        char[][] taulell = new char[3][3];
+        char[][] taulell= { {'·','·','·'},
+                            {'·','·','·'},
+                            {'·','·','·'}};
 
-        // inicialització de la fila '·'
-        taulell[0][0] = '·';
-        taulell[0][1] = '·';
-        taulell[0][2] = '·';
-
-        taulell[1][0] = '·';
-        taulell[1][1] = '·';
-        taulell[1][2] = '·';
-    
-        taulell[2][0] = '·';
-        taulell[2][1] = '·';
-        taulell[2][2] = '·';
-        /*char[][] taulell= {'·','·', '·'*/
-        
-        
         System.out.println("Comença el joc");
+        
         mostrataula(taulell);
-        System.out.println("X?");
+        
+        while(joc){
+        
+        String posicion = "";
+        
+        System.out.println("jugador + ?");
+        posicion = Entrada.readLine();
+        posicion= posicion.toLowerCase();
+            if(posicion.equals("a")){
+                System.out.println("el jugador  abandona la partida");/**/
+                break;
+            }else{
+                if(posicion.length() >= 2){
+                //primerMov= posicion.charAt(0)= linea
+                //segonMov= posicion.charAt(1)  column
+                }
+                    System.out.println("X?");
+            }
+        }
     }
+    
+
     public static void mostrataula(char[][] taulell){
-    for (int fil = 0; fil < 3; fil++) {
-        /*for(int col= 0; col < 3; col++)*/
-            System.out.println(taulell[fil]);
+    for(int fil= 0; fil < taulell.length; fil++) {
+        for(int col= 0; col < taulell.length; col++){
+            System.out.print(taulell[fil][col]);
         }
         System.out.println();
+      }
     }
 }
+
+
+
+
+/*
+switch(jo)
+     case"00":
+        taulell[0][0]='X';
+        break;
+    
+     case"01":
+        taulell[0][1]='X';
+        break;
+    
+     case"02":
+        taulell[0][2]='X';
+        break;
+    
+     case"10":
+        taulell[1][0]='X';
+        break;
+        
+     case"11":
+        taulell[1][1]='X';
+        break;
+    
+     case"12":
+        taulell[1][2]='X';
+        break;
+    
+     case"20":
+        taulell[2][0]='X';
+        break;
+        
+     case"21":
+        taulell[2][1]='X';
+        break;
+    
+     case"22":
+        taulell[2][2]='X';
+        break;
+
+
+
+
+
+
+
+
+*/
