7f92ad09a8a637ce8a42ecd768964ddaac7203ef
prgtest <cf22angelo.montenegro@iesjoandaustria.org>
2023-01-18 20:27:56 +0100

    recuperat de repo corrupte  ∾ prgtest autocommit 32_29 #1

--- 32_29_cadena_continua/UtilString.java
+++ 32_29_cadena_continua/UtilString.java
@@ -33,12 +33,13 @@ public class UtilString{
     public static boolean esEnter(String text){
         char mas ='+';
         char menos='-';
+        char espai= ' ';
         int longitud = text.length();
         boolean numero= true;
             if (!text.isEmpty()){
                 for(int i =0; i < longitud-1; i++){
                     if (!Character.isDigit(text.charAt(i))){
-                        if (!(i==0 && (text.charAt(0)== mas || text.charAt(0)== menos))){
+                        if (!(i==0 && (text.charAt(0)== mas || text.charAt(0)== menos || text.charAt(0)== espai))){
                          numero = false;
                         }
                     }
