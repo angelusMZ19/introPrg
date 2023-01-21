1013bd9d7089cccfe292a07ebc93723a9bf86ab6
prgtest <cf22angelo.montenegro@iesjoandaustria.org>
2023-01-21 16:36:00 +0100

    prova  ∾ prgtest resultat 32_54: PASSA

--- .introprg/tmp/test/Subcadenes.java
+++ .introprg/tmp/test/Subcadenes.java
@@ -1,13 +1,4 @@
-/*SIN USAR FOR , WHILE, DO WHILE
-En aquesta ocasió, desenvoluparàs un programa que obtindrà un text per entrada estàndard i mostrarà per sortida estàndard totes les subcadenes possibles, tenint en comptes només les lletres.
-Per mostrar l'ordre requerit per aquest exercici, caldrà que implementis el següent algorisme recursiu:
-Si la cadena d'entrada té una longitud menor o igual a 1, es mostra la cadena sencera i s'acaba
-Si la cadena té una longitud més gran que 1, llavors:
-    1- Primer es mostra la cadena
-    2- Després es mostra les subcadenes corresponents a la primera meitat de la cadena
-    3- Finalment es mostra les subcadenes corresponents a la resta de la cadena
-    
-*/
+/**/
 public class Subcadenes{
     public static void main (String[]args){
     
@@ -36,13 +27,16 @@ public class Subcadenes{
     }
     public static void divisionString(String text){
         if (text.length() <= 1){
-         /*1*/   System.out.printf("%4d: %s%n", text.length(), text);
-        }else{
             System.out.printf("%4d: %s%n", text.length(), text);
-            String priMitad= text.substring(0, text.length()/2);
-         /*2*/   divisionString(priMitad);
-            String segMitad= text.substring(text.length()/2);
-         /*3*/   divisionString(segMitad);
+        }else{
+        String priMitad= text.substring(0, text.length()/2);
+    /*2*/       divisionString(priMitad);
+        String segMitad= text.substring(text.length()/2);
+    /*3*/       divisionString(segMitad);
+    
+    /*1*/   System.out.printf("%4d: %s%n", text.length(), text);
+            
+            
         }
     }
 }
