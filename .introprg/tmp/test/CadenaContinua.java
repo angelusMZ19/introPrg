5b03d8e0446fc8cfebe7f8f1e2d9cb8a8dba6e69
prgtest <cf22angelo.montenegro@iesjoandaustria.org>
2023-01-22 17:54:35 +0100

    finalizado 32_29  ∾ prgtest resultat 32_29: FALLA

---
+++ .introprg/tmp/test/CadenaContinua.java
@@ -0,0 +1,38 @@
+/*
+El programa CadenaContinuahará lo prácticamente lo mismo que la versión original pero los cálculos los realizará una función llamada cadenaContinua()
+
+Sin embargo, la nueva versión será capaz de controlar el caso en que no introduzcan un número entero por la longitud de la cadena resultante.
+El programa CadenaContinuahará lo prácticamente lo mismo que la versión original pero los cálculos los realizará una función llamada cadenaContinua()
+
+Sin embargo, la nueva versión será capaz de controlar el caso en que no introduzcan un número entero por la longitud de la cadena resultante.
+
+en este ejercicio se usara dos funciones que estaran adjuntas dentro de un mismo archivo denominado utilString pero con dos modulos diferentes
+*/
+public class CadenaContinua{
+    public static void main(String[]args){
+    
+        System.out.println("Text?");
+        String text = Entrada.readLine();
+        /*text = text.strip();*/
+        if(!(text.isEmpty())){
+            System.out.println("Nombre?");
+            String longitud = Entrada.readLine();
+            longitud = longitud.strip();
+             
+            boolean enter= UtilString.esEnter(longitud);
+           
+            //String medida= String.valueOf(longitud);
+            //System.out.println(enter);
+            if(enter){
+            int valor= Integer.parseInt(longitud);/**/
+            String resultat = UtilString.cadenaContinua(text, valor);
+            System.out.println(resultat);
+            }else{
+                System.out.println("error");
+                }
+        }else{
+        System.out.println("error");
+        }
+    }
+}
+
