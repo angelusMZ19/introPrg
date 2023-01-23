b2389fa7c02b44395451acb011c9d14f1ae318e0
prgtest <cf22angelo.montenegro@iesjoandaustria.org>
2023-01-23 01:11:54 +0100

    Finalizado 32_50 programa realizado con recursividad, sin for ni while  ∾ prgtest resultat 32_50: PASSA

--- .introprg/tmp/test/TestExercise.java
+++ .introprg/tmp/test/TestExercise.java
@@ -1,50 +1,43 @@
 /*
- * Unit testing methods for an exercise
- */
+    Unit testing methods for exercise 02_37_mostralletres_rec
+*/
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestExercise {
 
-    @ParameterizedTest
-    @CsvSource(value={
-        "'', false",
-        "'   ', false",
-        "1,true",
-        "-1,true",
-        "+1,true",
-        "123,true",
-        "-456,true",
-        "-4a6,false",
-        "' 1',false",
-        "'1 ',false",
-    })
-    @DisplayName("test esEnter()")
-    public void esEnter(String text, boolean esperat) {
-        boolean obtingut = UtilString.esEnter(text);
-        assertEquals(esperat, obtingut, "Revisa el cas esEnter(\"" + text + "\")");
-    }
-
-    @ParameterizedTest
-    @CsvSource(value={
-    "danburi, 0, ''",
-    "danburi, 1, d",
-    "danburi, 2, da",
-    "danburi, 6, danbur",
-    "danburi, 7, danburi",
-    "danburi, 12,danburidanbu",
-    "danburi, 13,danburidanbur",
-    "danburi, 25,danburidanburidanburidanb"
-    })
-    @DisplayName("test cadenaContinua()")
-    public void intervalString(String text, int longitud, String esperat) {
-        String obtingut = UtilString.cadenaContinua(text, longitud);
-        assertEquals(esperat, obtingut, "Revisa el cas cadenaContinua(\"" + text + "\", " + longitud + ")");
+    @Test
+    @DisplayName("Test hi és mòdul mostraLletres()")
+    public void prgtestX1mostraLletres() {
+        Class classe = MostraLletres.class;
+        String modulObjectiu = "mostraLletres";
+        Method[] methods = classe.getDeclaredMethods();
+        boolean foundTarget = false;
+        for (Method method: methods) {
+            if (modulObjectiu.equals(method.getName())) {
+                foundTarget=true;
+                Type[] types = method.getGenericParameterTypes();
+                assertEquals(
+                        1, 
+                        types.length, 
+                        "Revisa els paràmetres requerits pel mòdul " + modulObjectiu + "()"
+                );
+                assertEquals(
+                    "java.lang.String",
+                    types[0].getTypeName(),
+                    "Revisa els paràmetres requerits pel mòdul " + modulObjectiu + "()"
+                );
+                assertEquals(
+                        "void",
+                        method.getGenericReturnType().getTypeName(),
+                        "S'esperava " + modulObjectiu + "() d'un tipus diferent"
+                );
+            }
+        }
+        assertTrue(foundTarget, "No es troba el mòdul " + modulObjectiu + "(). Revisa enunciat.");
     }
 }
