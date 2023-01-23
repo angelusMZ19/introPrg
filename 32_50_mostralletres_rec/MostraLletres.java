6d920fb5846ddff3963fd36d2444dce0f7619547
Angelo Enrique Montenegro Zavala <cf22angelo.montenegro@iesjoandaustria.org>
2023-01-23 01:11:50 +0100

    Finalizado 32_50 programa realizado con recursividad, sin for ni while

--- 32_50_mostralletres_rec/MostraLletres.java
+++ 32_50_mostralletres_rec/MostraLletres.java
@@ -1,30 +1,8 @@
-/**/
-/*public class MostraLletres{
+/*Este programa creara un bucle si usar funciones de bucles como el for, while, o do while
+sera un programa que muestre las letras a partir del texto solicitado */
+public class MostraLletres{
      public static void main(String[] args){
-         String text = Entrada.readLine() ;
-         int quantes = quantesLletres(text);
-         System.out.println("El text:");
-         System.out.println("\t\""+ text + "\"");
-         System.out.println("conté " +
-                            quantes +
-                            " lletr" +
-                            (quantes == 1 ? "a" : "es")
-                           );
-     }
-
-     public static int quantesLletres(String text) {
-         int comptador = 0;
-         for (int i = 0; i < text.length(); i++) {
-             char actual = text.charAt(i);
-             if (Character.isLetter(actual)) {
-                 comptador += 1;
-             }
-         }
-         return comptador;
-     }
-}*/
-
-/*
+ 
         System.out.println("Text?");
         String text = Entrada.readLine();
         mostraLletres(text);
@@ -42,48 +20,15 @@
         // considerem el primer caràcter
         char primer = text.charAt(0);
         if (Character.isLetter(primer)) {
-            letra = text.charAt(i);
-              cad = cad + letra;
+           System.out.print(primer);
         }
 
         // considerem les lletres que conté la resta del text
-        String restaText =  XXX ;  // resta del text
+        String restaText =  text.substring(1);  // resta del text
         mostraLletres(restaText);      // crida recursiva
 
     }
 }
-*/
-
-public class NomesLletres{
-    public static void main (String[]args){
-        
-        System.out.println("Text?");
-        String text= Entrada.readLine();
-        
-        char letra;
-        String cad= "";
-        boolean chequeo= false;
-        for(int i = 0; i < text.length(); i++){
-             if (Character.isLetter(text.charAt(i))){
-              letra = text.charAt(i);
-              cad = cad + letra;
-                if(chequeo == false){
-                    System.out.print(letra);
-                    chequeo = true;
-                }else{
-                    System.out.print(", " + letra);
-                } 
-            }
-        }
-        System.out.println();
-    }
-}
-
-
-
-
-
-
 
 
 
