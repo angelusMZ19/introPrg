0e1ffd1ca7e4d51d506f58dec0c6c36422c36a97
prgtest <cf22angelo.montenegro@iesjoandaustria.org>
2023-01-26 17:23:11 +0100

    finalizado 33_02es un programa similar al lloro inici fi solo que en lugar de leer datos desde el prompt los lee desde el un archivo que se llama dentro del codigo  ∾ prgtest resultat 33_02: PASSA

--- .introprg/tmp/test/TestExercise.java
+++ .introprg/tmp/test/TestExercise.java
@@ -2,49 +2,22 @@
  * Unit testing methods for an exercise
  */
 
-import java.lang.reflect.Method;
-import java.lang.reflect.Type;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
 
 public class TestExercise {
 
     @ParameterizedTest
     @CsvSource(value={
-        "'', false",
-        "'   ', false",
-        "1,true",
-        "-1,true",
-        "+1,true",
-        "123,true",
-        "-456,true",
-        "-4a6,false",
-        "' 1',false",
-        "'1 ',false",
-    })
-    @DisplayName("test esEnter()")
-    public void esEnter(String text, boolean esperat) {
-        boolean obtingut = UtilString.esEnter(text);
-        assertEquals(esperat, obtingut, "Revisa el cas esEnter(\"" + text + "\")");
+    "a, true", "à,true","e,true","è,true","é,true","i,true","í,true","ï,true","o,true","ó,true","ò,true","u,true","ú,true","ü,true",
+    "A, true", "À,true","E,true","È,true","É,true","I,true","Í,true","Ï,true","O,true","Ó,true","Ò,true","U,true","Ú,true","Ü,true",
+    "c,false","d,false","f,false","j,false"})
+    @DisplayName("test trobaOcurrencies()")
+    public void esVocal(char lletra, boolean esperat) {
+        boolean obtingut = UtilString.esVocal(lletra);
+        assertEquals(esperat, obtingut, "Revisa la crida esVocal('" + lletra + "')");
     }
 
-    @ParameterizedTest
-    @CsvSource(value={
-    "danburi, 0, ''",
-    "danburi, 1, d",
-    "danburi, 2, da",
-    "danburi, 6, danbur",
-    "danburi, 7, danburi",
-    "danburi, 12,danburidanbu",
-    "danburi, 13,danburidanbur",
-    "danburi, 25,danburidanburidanburidanb"
-    })
-    @DisplayName("test cadenaContinua()")
-    public void intervalString(String text, int longitud, String esperat) {
-        String obtingut = UtilString.cadenaContinua(text, longitud);
-        assertEquals(esperat, obtingut, "Revisa el cas cadenaContinua(\"" + text + "\", " + longitud + ")");
-    }
 }
