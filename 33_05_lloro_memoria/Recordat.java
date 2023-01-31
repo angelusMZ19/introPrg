a621c9ffc0d0803e31bc3d2ab68ec9cb94651b7c
Angelo Enrique Montenegro Zavala <cf22angelo.montenegro@iesjoandaustria.org>
2023-01-31 16:58:10 +0100

    prueba de pasar el prgtest

--- 33_05_lloro_memoria/Recordat.java
+++ 33_05_lloro_memoria/Recordat.java
@@ -1,4 +1,4 @@
-/*
+    /*
 programa que aplica la escritura de un archivo y usa este mismo para repetir lo que se a ingresado en el documento 
 es decir escribiremos un archivo y luego repetiremos su contenido
 */
@@ -7,7 +7,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.FileWriter;
 import java.io.BufferedWriter;
-import java.util.Scanner;
+
 
 public class Recordat{
     public static void main (String [] args) throws IOException{
@@ -17,42 +17,33 @@ public class Recordat{
         mostraRecords(nomFitxer);
     }
 
-    /**
-     * Demana frases per entrada estàndard i les guarda a un fitxer
-     * @param nomFitxer: nom del fitxer
-     */
     public static void processaEntrada(final String nomFitxer) throws IOException {
-        String text = "hola";
-        Scanner teclado = new Scanner(System.in);
-
-        while (!(text.isEmpty()) && !(text.isBlank())) {
+        String text= "hola";
+        BufferedWriter sortida = new BufferedWriter(new FileWriter(nomFitxer));
+        while(!(text.isEmpty()) && !(text.isBlank())){
         System.out.print("El lloro pregunta paraula: ");
-            text = teclado.nextLine();
-            FileWriter archivo = new FileWriter(text);
-            BufferedWriter sortida = new BufferedWriter(archivo);
-            
-            sortida.write(nomFitxer);
-                if(!(text.isEmpty()) && !(text.isBlank())){
-                    System.out.println("El lloro registra: " + text);
-
-                    sortida.close();
-              
-                }
-        
+            text = Entrada.readLine();
+            text = text.strip();
+            if(!(text.isEmpty()) && !(text.isBlank())){
+                System.out.println("El lloro registra: " + text);
+                    sortida.write(text);
+                    sortida.newLine();
+            }else{
+                System.out.println("cap palabra");
+            }
         }
-        System.out.println();
+        sortida.close();
     }
-     
     public static void mostraRecords(final String nomFitxer) throws IOException {
-        String alegre = nomFitxer;
-        FileReader file= new FileReader(alegre);
-        BufferedReader input= new BufferedReader(file);
-        
+        FileReader fitxer = new FileReader (nomFitxer);
+        BufferedReader input = new BufferedReader(fitxer);
         while (true) {
-            String palabra = input.readLine();
-            if (null == palabra) break;
-            System.out.println("El lloror recorda: "+ palabra);
+            String linia = input.readLine();
+            if (null == linia) break;
+            System.out.printf("El lloro recorda:%s%n",linia);
         }
         input.close();
+        System.out.println("Adeu");
     }
-}   
+}
+
--- ANGELO/recuerdos.java
+++ ANGELO/recuerdos.java
@@ -7,19 +7,17 @@ import java.io.FileWriter;
 import java.io.BufferedWriter;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.Scanner;
 
 public class recuerdos {
     public static void main(String[] args) throws IOException {
         String cami = "resultat.txt";
-        
-        String text = ".";
+         BufferedWriter sortida = new BufferedWriter(new FileWriter(cami));
         while (!(text.isEmpty()) && !(text.isBlank())) {
         System.out.print("El lloro pregunta paraula: ");
         text = Entrada.readLine();
             if(!(text.isEmpty()) && !(text.isBlank())){
              
-                BufferedWriter sortida = new BufferedWriter(new FileWriter(cami));
+               
                 System.out.println("El lloro registra: " + text);
                 sortida.write(text);
                 sortida.newLine();
