2d1837877e66773ce5c5460bb9c500c2b7214209
Angelo Enrique Montenegro Zavala <cf22angelo.montenegro@iesjoandaustria.org>
2023-02-04 12:07:38 +0100

    finalizado 32_21

--- 32_21_matricula_italiana/MatriculaValida.java
+++ 32_21_matricula_italiana/MatriculaValida.java
@@ -5,7 +5,41 @@ public class MatriculaValida{
     System.out.println("Introdui una matricula");
     String matricula= Entrada.readLine();
         if (matricula.length()==7){
-        
+        boolean correcta=true;
+            for(int i=0; i < matricula.length();i++){
+                char c= matricula.charAt(i);
+                    if(i==2 || i==3  || i==4 ){
+                        if(Character.isDigit(c)){
+                            correcta=true;
+                        }else{
+                            correcta= false;
+                        }
+                    }else{
+                        correcta= esLletraValidaPerMatriculaItaliana(c);
+                    }
+                    if(correcta){
+                        continue;
+                    }else{
+                        System.out.println("No és matrícula italiana válida");
+                        return;
+                    }
+            }
+            System.out.println("És una matrícula italiana válida");
+        }else {
+        System.out.println("No és matrícula italiana válida");
+        }
     }
+    public static boolean esLletraValidaPerMatriculaItaliana(char c){
+        
+        String lletres= "ABCDEFGHIJKLMNPRDTVWXYZ";
+        for(int j=0; j<lletres.length(); j++){
+            char v= lletres.charAt(j);
+            if(c==v){
+                return true;
+            }else{
+                continue;
+            }
+        }
+        return false;
     }
 }
