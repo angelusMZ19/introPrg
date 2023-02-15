62f54be08bcfddd88a181eda5f8b9522133e6bcb
Angelo Enrique Montenegro Zavala <cf22angelo.montenegro@iesjoandaustria.org>
2023-02-15 12:03:13 +0100

    finalizado 34_08 a modo de modulo se puso la declaracion del valor del piso a parte y se llamo al modulo principal

--- 34_08_desdefora/Ascensor.java
+++ 34_08_desdefora/Ascensor.java
@@ -1,4 +1,7 @@
 /*
+modulo creado para
+declarar la variables del piso para ser usado en el fichero de UsaAscensor
+uso de modulo identico a lo que se realizo en la uf2
 */
 public class Ascensor{
     public int pis= -1;
